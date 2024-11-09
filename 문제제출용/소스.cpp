925cd526849be0e81fa7ca651d5c01cf67133219
김건우 <gun2935@naver.com>
2024-11-09 11:43:11 +0900

    feat : 문장 출력 과목 점수

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,9 +4,11 @@
 int main()
 {
 
-	printf("수를 출력할 때는 따옴표를 생략합니다.\n");
-	printf("123\n");
-	printf("6.500000");
+	printf(" %s\n","subject score");
+	printf("==============\n");
+	printf("  %s    %d\n","korean", 90);
+	printf(" %s   %d\n", "english",100);
+	printf("%s    %d\n", "computer", 80);
 
 	return 0;
 
