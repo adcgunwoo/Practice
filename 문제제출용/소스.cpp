f556f1df8387d6af5e6deed4e3a12b8af7e6eca1
김건우 <gun2935@naver.com>
2024-11-09 11:12:14 +0900

    feat : 문장 출력 문자열 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,14 +4,9 @@
 int main()
 {
 
-	printf("   @@   \n");
-	printf("  @  @  \n");
-	printf(" @    @\n");
-	printf("@      @\n");
-	printf(" @    @\n");
-	printf("  @  @\n");
-	printf("   @@\n");
-
+	printf("서식 문자로 출력하는 방법입니다.\n");
+	printf("이것은 문자열을 나타냅니다.\n");
+	
 	return 0;
 
 }
