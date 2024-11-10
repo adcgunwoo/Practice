fb3824fd52286bd4c15914b60f2e7cc794289071
김건우 <gun2935@naver.com>
2024-11-10 14:09:02 +0900

    feat : 문자열 출력 각 열의 크기를 설정해 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,14 +4,13 @@
 int main()
 {
 
-	int a;
-	char b;
+	int a, b, c;
 
-	a = 10;
-	b = 'A';
-
-	printf("a = %d\n", a);
-	printf("b = %c", b);
+	printf("%15s%15s%15s\n", "Seoul","10,312,545","+91,375");
+	printf("%15s%15s%15s\n", "Pusan", "3,567,910", "+5,868");
+	printf("%15s%15s%15s\n", "Incheon", "2,758,296", "+64,888");
+	printf("%15s%15s%15s\n", "Daegu", "2,511,676", "+17,230");
+	printf("%15s%15s%15s\n", "Gwangju","1,454,636","+29,774");
 
 	return 0;
 
