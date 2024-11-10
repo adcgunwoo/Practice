86d89d2253bf642d78704b6fb397344366bceccd
김건우 <gun2935@naver.com>
2024-11-10 20:13:40 +0900

    feat : 연산자 사칙연산 및 나머지 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,19 +1,14 @@
-#define _crt_secure_no_warnings
+#define _crt_no_secure_warning
 #include <stdio.h>
 
 int main()
 {
-	int a, b;
-	printf("두 개의 수를 입력하시오. ");
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %d\n", a, b, a + b);
-	printf("%d - %d = %d\n", a, b, a - b);
-	printf("%d * %d = %d\n", a, b, a * b);
-	printf("%d / %d = %d\n", a, b, a / b);
-	printf("%d %% %d = %d", a, b, a % b);
+	int a, b, c, d, e;
 
-	//%를 문자로 나타내고 싶으면 %% 붙여쓰기
+	printf("5개의 수를 입력하시오. ");
+	scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+	printf("%d %d %d %d %d", a + 3, b - 3, c * 3, d / 3, e % 3);
+
 
 	return 0;
-
 }
