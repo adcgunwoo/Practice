1b4240bafb6821eb967560c8b11111482f0235dd
김건우 <gun2935@naver.com>
2024-11-10 14:17:55 +0900

    feat : 문장 입출력 \n을 사이에 둬도 줄 바꾸기가 잘 되는구나

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,14 +4,13 @@
 int main()
 {
 
-	int a, b, c;
+	int a, b;
 
-	printf("%15s%15s%15s\n", "Seoul","10,312,545","+91,375");
-	printf("%15s%15s%15s\n", "Pusan", "3,567,910", "+5,868");
-	printf("%15s%15s%15s\n", "Incheon", "2,758,296", "+64,888");
-	printf("%15s%15s%15s\n", "Daegu", "2,511,676", "+17,230");
-	printf("%15s%15s%15s\n", "Gwangju","1,454,636","+29,774");
+	a = -1;
+	b = 100;
 
+	printf("%d\n%d",a,b);
+	
 	return 0;
 
 }
