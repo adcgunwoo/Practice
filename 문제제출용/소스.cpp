d1e9257521772b50cf2bba565be81bee0ab61795
김건우 <gun2935@naver.com>
2024-11-10 17:07:58 +0900

    feat : 대입 연산자 +-*/% 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,13 +3,17 @@
 
 int main()
 {
-	int a,b;
-
-	printf("두 수를 입력하시오. ");
-	scanf("%d %d", &a,&b);
-	printf("%d + %d = %d\n",a,b,a+b);
+	int a, b;
+	printf("두 개의 수를 입력하시오. ");
+	scanf("%d %d", &a, &b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
 	printf("%d * %d = %d\n", a, b, a * b);
-	
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d %% %d = %d", a, b, a % b);
+
+	//%를 문자로 나타내고 싶으면 %% 붙여쓰기
+
 	return 0;
 
 }
