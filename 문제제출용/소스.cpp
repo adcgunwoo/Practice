a087926435a6b3a0ec9840da922a82b2fbf8a657
김건우 <gun2935@naver.com>
2024-11-10 13:59:18 +0900

    feat : 변수 출력 십진 정수 및 문자 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,11 +4,14 @@
 int main()
 {
 
-	printf("%8s%6s\n","subject","score");
-	printf("==============\n");
-	printf("%8s%6d\n","korean", 90);
-	printf("%8s%6d\n","english",100);
-	printf("%8s%6d\n","computer", 80);
+	int a;
+	char b;
+
+	a = 10;
+	b = 'A';
+
+	printf("a = %d\n", a);
+	printf("b = %c", b);
 
 	return 0;
 
