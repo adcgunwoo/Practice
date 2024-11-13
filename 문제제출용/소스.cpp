47b473f50485e61085053c5224383dc96c6b4001
김건우 <gun2935@naver.com>
2024-11-13 14:11:27 +0900

    feat : 연산자 숫자 비교 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,14 +3,17 @@
 
 int main()
 {
-	int a,b,c;
+	int a, b, c;
+	int result1, result2, result3, result4;
 
-	scanf("%d %d", &a, &b);
-
-	a = ++a;
-	c = a + b--;
-
-	printf("a = %d, b = %d, c = %d", a, b, c);
+	scanf("%d %d %d", &a, &b, &c);
 	
+	result1 = (a > b);
+	result2 = (b >= c);
+	result3 = (a <= b);
+	result4 = (b < c);
+
+	printf("%d %d %d %d", result1, result2, result3, result4);
+
 	return 0;
 }
