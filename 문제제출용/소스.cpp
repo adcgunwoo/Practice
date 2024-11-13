979c1dcaef5dca186c8139970025e72459dfdccd
김건우 <gun2935@naver.com>
2024-11-13 17:19:11 +0900

    feat : 디버깅 디버깅 창에서 화살표가 가리키는 값 구하기

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,19 +3,10 @@
 
 int main()
 {
-	double a, b; //실수형 변수
-	int d, e;
-	
-	scanf("%lf %lf", &a, &b);
-
-	d = a + b;
-
-	int f, g;
-	f = int(a);
-	g = int(b);
-
-	e = f + g;
-	printf("%d %d", d, e);
+	int a = 5; // 디버깅에서 아직 입력 X (쓰레기값)
+	a += 10; // 디버깅에서 a의 값 5
+	a = a - 1; //디버깅에서 a의 값 15
+	printf("%d\n", 15); 
 
 
 	return 0;
