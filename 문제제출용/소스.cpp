621412e32c9631cb9c09f04ec670197fc5bdc10e
김건우 <gun2935@naver.com>
2024-11-13 18:03:17 +0900

    feat : if문(조건선택문) , 선택제어문 if안의 조건이 참이면 실행, 거짓이면 건너뛴다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,18 +1,14 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <time.h> // 시간 함수
 int main()
 {
-	int a = 0;
-	time_t now;
-	struct tm tt;
-	time(&now);
-	tt = *localtime(&now);
-	a = tt.tm_year; // 1
-	a += tt.tm_mon; // 2
-	a += tt.tm_mday;
-	printf("%d %d %d\n", 0,124,147); // 3
+	int a;
 
+	scanf("%d", &a);
+	printf("%d\n", a);
+	if (a > 10) {
+		printf("10보다 큰 수를 입력하셨습니다.");
+	}
 
 	return 0;
 }
