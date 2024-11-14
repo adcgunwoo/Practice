02876a6af5bedfa2d7498ffd1be28741bc7e945a
김건우 <gun2935@naver.com>
2024-11-14 16:09:04 +0900

    feat : 윤년(leap year), 평년(common year) 계산 선택제어문으로 조건을 나누어 계산 및 구현

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,11 +2,15 @@
 #include <stdio.h>
 int main()
 {
-	int a, b;
-	int max_data;
-	scanf("%d %d", &a, &b);
+	int a;
 
-	max_data = (a > b) ? a : b;
-	
+	scanf("%d", &a);
+	if (a % 400 == 0 || (a%4==0 && a%100!=0)) {
+		printf("leap year");
+	}
+	else {
+		printf("common year");
+	}
+
 	return 0;
 }
