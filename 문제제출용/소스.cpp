fdbed501e6b7be6885b14f3c85ae1e610e14fd87
김건우 <gun2935@naver.com>
2024-11-14 15:34:48 +0900

    feat : 조건연산자 in 선택제어문 식3 = 조건식 ? 식1 : 식2 의 형식으로 식1이 참이면 식3에 식1을 대입, 거짓이면 식2를 대입한다. 코드를 간결하게 해주지만, if문으로 더 알기 쉽게 표현할 수 있기에 잘 쓰이진 않는다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,29 +2,11 @@
 #include <stdio.h>
 int main()
 {
-	int a;
-	printf("점수를 입력하세요. ");
-	scanf("%d", &a);
+	int a, b;
+	int max_data;
+	scanf("%d %d", &a, &b);
 
-	switch (a / 10) 
-	{
-	case 10:
-	case 9:
-		printf("A");
-		break;
-	case 8:
-		printf("B");
-		break;
-	case 7:
-		printf("C");
-		break;
-	case 6:
-		printf("D");
-		break;
-	default:
-		printf("F");
-		break;
-	}
+	max_data = (a > b) ? a : b;
 	
 	return 0;
 }
