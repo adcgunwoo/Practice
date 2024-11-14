7f023945c3b95cc9318eb5b67f27ba76e7fd8dc4
김건우 <gun2935@naver.com>
2024-11-14 17:12:25 +0900

    feat : 반복제어문 while (1) 조건문 ()안에 있는 내용이 1, 즉탈출 명령이 없으면 프로그램이 무한반복 된다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,10 +2,21 @@
 #include <stdio.h>
 int main()
 {
-	char alpha = 'A';
-	while (alpha <= 'Z') {
-		printf("%c", alpha++);
+	int score;
+	while (1) {
+		printf("점수를 입력하세요. ");
+		scanf("%d", &score);
+		if (score >= 80 && score <=100) {
+			printf("축하합니다. 합격입니다.\n");
+		}
+		else if (score >= 0 && score < 80) {
+			printf("죄송합니다. 불합격입니다.\n");
+		}
+		else {
+			break;
+		}
 	}
+	
 
 	return 0;
 }
