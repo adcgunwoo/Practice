0a90d313822a561a1a7f446cf86d1750ea1fdfed
김건우 <gun2935@naver.com>
2024-11-15 17:42:11 +0900

    feat : 다중반복문을 예시 2 다중반복문을 이용한 구구단 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,11 +2,11 @@
 #include <stdio.h>
 int main()
 {
-	int i,j, n;
+	int i,j;
 	
-	for (i = 2; i <= 6; i++) {
-		for (j = i; j <= i+4; j++) {
-			printf("%d ", j);
+	for (i = 1; i <= 9; i++) {
+		for (j = 2; j <= 4; j++) {
+			printf("%d * %d = %2d   ", j,i,j*i);
 		}
 		printf("\n");
 	}
