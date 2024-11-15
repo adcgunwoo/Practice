4468412a39f2c39a37224b573250aa4a34471648
김건우 <gun2935@naver.com>
2024-11-15 17:20:00 +0900

    feat : 다중반복문 반복문 안에 반복문이 또 있는 것을 말함 i,j를 변수로 선언해서 코드를 작성함

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,25 +2,20 @@
 #include <stdio.h>
 int main()
 {
-	int i,a;
-
-	int cnt3 = 0,cnt5=0;
-
-	for (i = 1; i <= 10; i++) {
-		scanf("%d", &a);
-		if (a % 15 == 0) {
-			cnt3++;
-			cnt5++;
+	int i,j, n;
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+			printf("%d ", i);
 		}
-		else if (a % 3 == 0) {
-			cnt3++;
-		}
-		else if (a % 5 == 0) {
-			cnt5++;
-		}
+		printf("\n");
 	}
-	printf("Multiples of 3 : %d\n", cnt3);
-	printf("Multiples of 5 : %d\n", cnt5);
+	printf("\n");
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
