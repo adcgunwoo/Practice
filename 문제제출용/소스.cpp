c6df9771f701fec6d12a8754aeed733d5c86856b
김건우 <gun2935@naver.com>
2024-11-15 15:41:13 +0900

    feat : %c 앞에 공백 in 반복제어문 형성평가 엔터, 공백과 같은 구분자를 입력받지 않으려면 %c 앞에 공백을 넣어줘야 함.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,19 +2,23 @@
 #include <stdio.h>
 int main()
 {
-	int a,avg;
-	int cnt = 0, sum = 0;
-	
-	do {
+	int a, b;
+	double area;
+	char trial;
+
+	while (1) {
+		printf("Base = ");
 		scanf("%d", &a);
-		if (a % 2 != 0) {
-			sum += a;
-			cnt++;
+		printf("Height = ");
+		scanf("%d", &b);
+		area = a * b / 2.0;
+		printf("Triangle width = %.1lf\n", area);
+		printf("Continue? ");
+		scanf(" %c", &trial);
+		if (trial != 'Y' && trial != 'y') {
+			break;
 		}
-	} while (a != 0);
-	avg = sum/cnt;
-	printf("È¦¼öÀÇ ÇÕ = %d\n", sum);
-	printf("È¦¼öÀÇ Æò±Õ = %d", avg);
+	}
 
 	return 0;
 }
