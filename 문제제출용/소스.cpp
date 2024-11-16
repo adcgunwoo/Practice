628bd0310f51b180d6e0cd0079c8b876c0607737
김건우 <gun2935@naver.com>
2024-11-16 16:04:01 +0900

    feat : 조건식이 없는 for문 II 연습문제는 조건을 초과시 멈추고 출력이었으나, 이 문제는 홀수의 합이 입력값 이상시 멈추고 출력이므로 같을 때에는 홀수의 개수를 추가 시키면 안되므로 if문 뒤로 식을 미루어주었다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,17 +2,19 @@
 #include <stdio.h>
 int main()
 {
-	int i,sum=0, j,a;
+	int i,sum=0, j,a,odd_cnt=0;
 	scanf("%d", &a);
 
 	for (i = 1;; i++) {
-		sum += i;
-		if (sum > a) {
-			printf("%d %d", i, sum);
-			break;
+		if (i % 2 != 0) {
+			if (sum >= a) {
+				printf("%d %d", odd_cnt, sum);
+				break;
+			}
+			sum += i;
+			odd_cnt++;
 		}
 	}
-	
 
 	return 0;
 }
