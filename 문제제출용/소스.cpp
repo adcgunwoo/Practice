a74bdd0338c21e7a46b75fff6c439b4ad3356912
김건우 <gun2935@naver.com>
2024-11-16 17:21:14 +0900

    feat : 피라미드 * 만들기 in 반복제어문 III 하나씩 추가되는것이 아닌 일정한 비율로 추가되기에 방정식으로 별의 개수를 구하는 식을 만들어야 함

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,20 +2,17 @@
 #include <stdio.h>
 int main()
 {
-	int i, j,a;
+	int a, i, j, k;
 
-	scanf("%d", &a);
-
-	for (i = 1; i <= a; i++) {
-		for (j = a-i; j >= 1; j--) {
+	for (i = 1; i <= 5; i++) {
+		for (j = i; j < 5; j++) {
 			printf(" ");
 		}
-		for (j = 1; j <= i; j++) {
+		for (j = 1; j <= 2*i-1; j++) {
 			printf("*");
 		}
 		printf("\n");
-	}
-
+	} //연습문제 5-1
 
 	return 0;
 }
