95712b9202f3c62d07598dcb4cafd42e846ee462
김건우 <gun2935@naver.com>
2024-11-17 16:40:42 +0900

    feat : for문 안에서 입력 받을 때 in 반복제어문 III 십진 정수를 입력 받을때 %d 뒤에 공백을 넣었더니, 입력이 되지 않았음 유의해야겠음

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,19 +3,18 @@
 int main()
 {
 	int i, j, a;
+	int sum = 0, cnt = 0;
+	int avg;
 
-	int num = 1;
-	char alpha = 'A';
-
-	for (i = 1; i <= 4; i++) {
-		for (j = 1; j <= i; j++) {
-			printf("%c ", alpha++);
+	for (i = 1; i <= 20; i++) {
+		scanf("%d", &a);
+		if (a == 0) {
+			break;	
 		}
-		for (j = 4; j >= i; j--) {
-			printf("%d ", num++);
-		}
-		printf("\n");
+		sum += a;
+		cnt++;	
 	}
-
+	avg = sum / cnt;
+	printf("%d %d", sum, avg);
 	return 0;
 }
