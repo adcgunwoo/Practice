38ab3cfc867f3f4c456a7b292250290b343c1ff3
김건우 <gun2935@naver.com>
2024-11-18 14:58:49 +0900

    feat : 배열 1-1 5개의 변수를 각각 설정해서 입력받는다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,22 +2,10 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, a;
-	int num = 1;
-	scanf("%d", &a);
+	int i, a1,a2,a3,a4,a5;
 
-	for (i = 1; i <= a; i++) {
-		for (j = 1; j <= a; j++) {
-			printf("%d ", num);
-			num += 2;
-			if (num > 10) {
-				num = 1;
-			}
-		}
-		printf("\n");
-	}
-	
-
+	scanf("%d %d %d %d %d", &a1, &a2, &a3, &a4, &a5);
+	printf("%d %d %d %d %d", a1, a2, a3, a4, a5);
 
 	return 0;
 }
