76df85b947b58b73576d0d78a9a142c8801974af
김건우 <gun2935@naver.com>
2024-11-18 17:32:55 +0900

    feat : 배열 뿐만 아니라 입력받을 때 &좀 빠뜨리지 말자.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,17 +1,25 @@
-#define _crt_no_secure_warnings
+#define _crt_no_sesure_warnings
 #include <stdio.h>
+
 int main()
 {
-	int i, min, a[10];
+	int i,a[10],even_max=0,odd_min=0;
 
-	min = 1001;
 	for (i = 0; i < 10; i++) {
 		scanf("%d", &a[i]);
-		
-		if (a[i] < min) {
-			min = a[i];
+		if (a[i] % 2 == 0) {
+			if (a[i] > even_max) {
+				even_max = a[i];
+			}
+		}
+		else if (a[i] % 2 != 0) {
+			if (a[i] < odd_min) {
+				odd_min = a[i];
+			}
 		}
 	}
-	printf("%d", min);
+
+	printf("%d %d", odd_min, even_max);
+
 	return 0;
 }
