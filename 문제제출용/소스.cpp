22e68ee2bb030b735f808f689d9b76e6980b10a4
김건우 <gun2935@naver.com>
2024-11-18 17:12:06 +0900

    feat : 배열에서의 평균 구하기 실수형 배열에 각 반 평균점수를 대입해 선언한다. 두 반을 입력받고, 선언한 sum에 두 반의 평균점수의 합을 대입하고 출력한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,34 +2,14 @@
 #include <stdio.h>
 int main()
 {
-	int i, j,year,month;
-	int days[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 }; 
-
-	while (1) {
-		printf("YEAR = ");
-		scanf("%d", &year);
-		printf("MONTH = ");
-		scanf("%d", &month);
-		if (month == 0) {
-			break;
-		}
-		else if (month > 12 || month < 1) {
-			printf("잘못 입력하였습니다.\n");
-		}
-		else if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-			if (month == 2) {
-				printf("입력하신 달의 날 수는 29일입니다.\n");
-			}
-			else {
-				printf("입력하신 달의 날 수는 %d일입니다.\n", days[month]);
-			}
-		}
+	int i,a,b;
+	double sum=0,avg[6] = { 85.6,79.5,83.1,80.0,78.2,75.0 };
 	
-		else {
-			printf("입력하신 달의 날 수는 %d일입니다.\n", days[month]);
-		}
-		printf("\n");
-	}
+
+	scanf("%d %d", &a, &b);
+	sum = avg[a - 1] + avg[ b - 1 ];
+
+	printf("%.1lf", sum);
 
 	return 0;
 }
