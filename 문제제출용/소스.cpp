f218ecebce3182e1ffb2b28cf08c735737418d4c
김건우 <gun2935@naver.com>
2024-11-18 15:15:17 +0900

    feat : 배열 1-3 a[n] 이라는 정수형 변수, 배열을 선언하고 반복문으로 입력받고, 출력한다. 일일히 많은 변수를 선언하고, 작성할 필요가 없어 편하다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,8 +4,12 @@ int main()
 {
 	int i, a[5];
 
-	scanf("%d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4]);
-	printf("%d %d %d %d %d", a[0], a[1], a[2], a[3], a[4], a[5]);
-	
+	for (i = 0; i < 5; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (i = 0; i < 5; i++) {
+		printf("%d ", a[i]);
+	}
+
 	return 0;
 }
