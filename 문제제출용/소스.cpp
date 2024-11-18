0db21ba1b86ad385dd2bbc9f61990818fbaa27dc
김건우 <gun2935@naver.com>
2024-11-18 15:39:35 +0900

    feat : 배열 헷갈릴 수 있는 점 배열은 0부터 입력 받기에, 배열의 n번째 값을 출력하려면 a[n-1]의 값을 출력해야 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,17 +2,12 @@
 #include <stdio.h>
 int main()
 {
-	int i;
-	char a[26],alpha='A';
-	for (i = 0; i < 26; i++) {
-		a[i] = alpha++;
-		//a[i]='A' + i 의 형태로도 작성 가능
-	}
-	for (i = 25; i >= 0; i--) {
-		printf("%c ", a[i]);
-	}
+	int i,a[10];
 	
-	
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &a[i]);
+	}
+	printf("%d %d %d", a[2], a[4], a[9]);
 
 	return 0;
 }
