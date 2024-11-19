5f254edb51138a70eec72dc9c417ade5832d9331
김건우 <gun2935@naver.com>
2024-11-19 14:44:38 +0900

    feat : 버블 정렬 원소가 거품처럼 올라오는 듯해 버블정렬이라고 불린다. 하나의 배열에서 바꾸게 되므로, 복잡도는 간단하다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,23 +3,23 @@
 
 int main()
 {
-	int i, j,a[10],tmp;
+	int i, j,a[5],tmp;
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 5; i++) {
 		scanf("%d", &a[i]);
 	}
 
-	for (i = 0; i < 9; i++) {
-		for (j = i + 1; j < 10; j++) {
-			if (a[i] < a[j]) {
-				tmp = a[i];
-				a[i] = a[j];
-				a[j] = tmp;
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 4-i; j++) {
+			if (a[j] < a[j+1]) {
+				tmp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = tmp;
 			}
 		}
 	}
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 5; i++) {
 		printf("%d ", a[i]);
 	}
 
