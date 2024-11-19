d1d2874d2d6eebdbdab1d46e9849864cdba6bfc6
김건우 <gun2935@naver.com>
2024-11-19 15:12:33 +0900

    feat : 삽입정렬 맞는 위치에 삽입시켜가며 정렬시키는 방법이다. 키값을 설정해서 조건에 만족하면 그 배열에 들어가 원래 있던 배열을 뒤로 밀어낸다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -8,17 +8,19 @@ int main()
 	for (i = 0; i < 5; i++) {
 		scanf("%d", &a[i]);
 	}
-
-	for (i = 0; i < 5; i++) {
-		for (j = 0; j < 4-i; j++) {
-			if (a[j] < a[j+1]) {
-				tmp = a[j];
-				a[j] = a[j+1];
-				a[j+1] = tmp;
+	for (i = 0; i < 4; i++) {
+		tmp = a[i + 1]; //시작점이 되는 값 또는 Key값
+		for (j = i; j > -1; j--) {
+			if (a[j] > tmp) {
+				a[j + 1] = a[j];
+			}
+			else {
+				break;
 			}
 		}
+		a[j + 1] = tmp;
 	}
-
+	
 	for (i = 0; i < 5; i++) {
 		printf("%d ", a[i]);
 	}
