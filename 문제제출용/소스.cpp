3ab7e6124ec1184a4c852f3acb9c41877fbad53e
김건우 <gun2935@naver.com>
2024-11-20 14:32:42 +0900

    feat : 배열과 선택정렬을 이용한 최대값, 최소값 출력 선택정렬을 이용해 내림차순으로 배열을 정렬했고, 배열의 처음값이 최대값, 999 바로 앞의 값이 최소값이 되었다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,19 +3,26 @@
 
 int main()
 {
-	int i=0;
-	char a[6] = { 'J','U','N','G','O','L'},alpha;
-	scanf(" %c", &alpha);
-	
-	for (i = 0; i < 6; i++) {
-		if (alpha == a[i]) {
-			printf("%d", i);
+	int i,j,k,a[100],tmp;
+	for (i = 0; i < 100; i++) {
+		scanf("%d", &a[i]);
+		if (a[i] == 999) {
 			break;
 		}
 	}
-	if (i > 5) {
-		printf("none");
+
+	for (j = 0; j < i - 1; j++) {
+		for (k = j+1; k < i; k++) {
+			if (a[j] < a[k]) {
+				tmp = a[j];
+				a[j] = a[k];
+				a[k] = tmp;
+			}
+		}
 	}
+	
+	printf("max : %d\n", a[0]);
+	printf("min : %d\n", a[i-1]);
 
 	return 0;
 }
