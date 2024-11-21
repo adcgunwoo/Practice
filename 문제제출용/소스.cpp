d29d80126477cf2e3d0944310f30cb45ca1dc155
김건우 <gun2935@naver.com>
2024-11-21 14:15:32 +0900

    feat : 세 명의 세 과목의 점수의 합 구하기 이차원 배열 [4][4]={{0}} 를 선언하고, 0부터 2까지 입력받고, [i][3]와 [3][j] 그리고 [3][3]에 합을 입력하고 출력했다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,29 +3,32 @@
 
 int main()
 {
-	int i, j, a[2][4], b[2][4];
-	
-	printf("first array\n");
+	int i, j,a[4][4]={{0}};
 
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 4; j++) {
+	for (i = 0; i < 3; i++) {
+		printf("%d번째 학생의 점수 ",i+1);
+		for (j = 0; j < 3; j++) {
 			scanf("%d", &a[i][j]);
+			a[i][3] += a[i][j];
+			a[3][j] += a[i][j];
+			a[3][3] += a[i][j];
 		}
 	}
 
-	printf("second array\n");
-	
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 4; j++) {
-			scanf("%d", &b[i][j]);
+	printf("     국어 영어 수학 총점\n");
+	for (i = 0; i < 4; i++) {
+		if (i >= 3) {
+			printf("합계");
 		}
-	}
-
-	for (i = 0; i < 2; i++) {
+		else {
+			printf(" %d번", i + 1);
+		}
 		for (j = 0; j < 4; j++) {
-			printf("%d ", a[i][j] * b[i][j]);
+			printf("%4d", a[i][j]);
 		}
 		printf("\n");
 	}
+
+
 	return 0;
 }
