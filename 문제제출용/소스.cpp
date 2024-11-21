9defcfcafe214f2443bd16d2ec66dedcce3f33c4
김건우 <gun2935@naver.com>
2024-11-21 18:12:27 +0900

    feat : 파스칼의 삼각형 변형 배열 II 뒤집힌 파스칼의 삼각형이다. 출력만 반대로 진행하면 된다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,28 +3,23 @@
 
 int main()
 {
-	int sum=0,i, j, width[4] = { 0 }, height[2] = { 0 }, a[4][2];
+	int n, i, j, a[11][11]={{0}};
+	a[1][1] = 1;
 
+	scanf("%d", &n);
 
-	for (i = 0; i < 4; i++) {
-		for (j = 0; j < 2; j++) {
-			scanf("%d", &a[i][j]);
-			width[i] += a[i][j];
-			height[j] += a[i][j];
-			sum+=a[i][j];
+	for (i = 2; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
 		}
 	}
 
-	for (i = 0; i < 4; i++) {
-		printf("%d ", width[i]/2);
+	for (i = n; i >=1 ; i--) {
+		for (j = i; j >= 1; j--) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
 	}
-	printf("\n");
-
-	for (i = 0; i < 2; i++) {
-		printf("%d ", height[i]/4);
-	}
-	printf("\n");
-	printf("%d", sum / 8);
 
 	return 0;
 }
