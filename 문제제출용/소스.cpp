7192a96b891d9c6a91cb14c71ebe3b3f5d1575d4
김건우 <gun2935@naver.com>
2024-11-21 17:44:21 +0900

    feat : 4행 2열의 가로,세로,전체 평균 구하기 in 배열 II 가로,세로평균을 저장할 배열과 전체 평균을 구할 정수 변수를 선언하고, 반복문으로 출력이 한꺼번에 안되기에, 따로 출력을 진행 해주어야 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,24 +3,28 @@
 
 int main()
 {
-	int i, j, /*int */ a[7][7] = {{0}};
+	int sum=0,i, j, width[4] = { 0 }, height[2] = { 0 }, a[4][2];
 
-	a[1][1] = 1; 
-	a[1][3] = 1; 
-	a[1][5] = 1;
 
-	for (i = 2; i < 6; i++) {
-		for (j = 1; j < 6; j++) {
-			a[i][j] = a[i-1][j-1]+a[i-1][j+1];
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 2; j++) {
+			scanf("%d", &a[i][j]);
+			width[i] += a[i][j];
+			height[j] += a[i][j];
+			sum+=a[i][j];
 		}
 	}
 
-	for (i = 1; i < 6; i++) {
-		for (j = 1; j < 6; j++) {
-			printf("%d ", a[i][j]);
-		}
-		printf("\n");
+	for (i = 0; i < 4; i++) {
+		printf("%d ", width[i]/2);
 	}
+	printf("\n");
+
+	for (i = 0; i < 2; i++) {
+		printf("%d ", height[i]/4);
+	}
+	printf("\n");
+	printf("%d", sum / 8);
 
 	return 0;
 }
