0b710022d2568525756d86bfda43b00f30696c06
김건우 <gun2935@naver.com>
2024-11-21 16:02:29 +0900

    feat  : 전전항과 전항의 합을 구하는 수열 in 배열 II 첫번째와 두번째 항을 입력받은 값으로 초기화하고, 반복문은 3번째 항부터 실행하게 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,23 +1,21 @@
-#define _crt_no_sesure_warnings
+#define _crt_no_secure_warnings
 #include <stdio.h>
 
 int main()
 {
-	int i,a[101],b[11]={0}; //범위 잘 볼것
+	int i,j,m, n, a[10]={0};
 
-	for (i = 0; i < 101; i++) {
-		scanf("%d", &a[i]);
-		if (a[i] == 0) {
-			break;
-		}
-		b[a[i] / 10]++;
-	}
-	for (i = 10; i >= 0; i--) { 
-		if (b[i] > 0) {
-			printf("%d : %d person\n", i * 10, b[i]);
+	scanf("%d %d", &m, &n);
+	a[0] = m, a[1] = n;
+
+	for (i = 2; i < 10; i++) {
+		a[i] = a[i - 2] + a[i - 1];
+		if (a[i] >= 10) {
+			a[i] = a[i] % 10;
 		}
 	}
-
-
+	for (i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
+	}
 	return 0;
 }
