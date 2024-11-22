cb788095450e5320829772b98a04e6ac3f97cdaa
김건우 <gun2935@naver.com>
2024-11-22 14:34:52 +0900

    feat : 매개변수 in 함수1 메인에서 입력받은 수를 입력받고, 전달되는 값을 인수라고 하며, 전달 받은 값을 저장하는 변수를 매개변수라고 한다. 각 함수의 선언된 변수는 각각 다르다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,16 +1,17 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-void line();
+void line(int num); //프로토타입(함수원형)선언
 int main()
 {
-	line();
-	puts("line 함수를 호출하였습니다.");
-	puts("line 함수를 다시 호출합니다.");
-	line();
+	int a;
+	scanf("%d", &a);
+	line(a); //a를 인수로 전달하여 line함수를 호출
 
 	return 0;
 }
-void line()
+void line(int num) //매개변수로 받아서 num에 저장
 {
-	puts("==============================");
+	printf("10큰수 : %d\n", num + 10);
+	printf("10작은수 : %d\n", num - 10);
 }
+
