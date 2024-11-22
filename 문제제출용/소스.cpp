8f91c07d812ca51f75b3b2b99c5af2340411b96e
김건우 <gun2935@naver.com>
2024-11-22 18:21:13 +0900

    feat : 전역변수 어떤 함수에도 속하지 않은 독립적으로 선언된 변수를 전역변수라고 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,25 +1,30 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-void gyesan(int& x, int& y);
+int a, b;
+int sum , mul;
+void question()
+{
+	printf("두 수를 입력하세요. ");
+}
+void input()
+{
+	scanf("%d %d", &a, &b);
+}
+void gyesan()
+{
+	sum = a + b;
+	mul = a * b;
+}
+void output()
+{
+	printf("합 : %d\n", sum);
+	printf("곱 : %d\n", mul);
+}
 int main()
 {
-	int a, b;
-	scanf("%d %d", &a, &b);
-	gyesan(a, b);
-
-	printf("%d %d", a, b);
+	question();
+	input();
+	gyesan();
+	output();
 	return 0;
 }
-void gyesan(int& x, int& y)
-{
-	if (x > y) {
-		x=x / 2;
-		y=y * 2;
-	}
-	else {
-		x=x * 2;
-		y=y / 2;
-	}
-
-}
-
