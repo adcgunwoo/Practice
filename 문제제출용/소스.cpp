f324fd0bc3de9f640bfa4ed22d425ea3618e2ac9
김건우 <gun2935@naver.com>
2024-11-22 14:44:50 +0900

    feat : 함수를 호출해 원의 넓이 구하기 메인에서 입력받은 값을 인수로 전달해, 호출된 함수는 매개변수로 원의 넓이를 구한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,17 +1,16 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-void line(int num); //프로토타입(함수원형)선언
+void area(int a);
 int main()
 {
-	int a;
-	scanf("%d", &a);
-	line(a); //a를 인수로 전달하여 line함수를 호출
+	int r;
+	scanf("%d", &r);
+	area(r);
 
 	return 0;
 }
-void line(int num) //매개변수로 받아서 num에 저장
+void area(int a)
 {
-	printf("10큰수 : %d\n", num + 10);
-	printf("10작은수 : %d\n", num - 10);
+	printf("%.2lf", a * a * 3.14);
 }
 
