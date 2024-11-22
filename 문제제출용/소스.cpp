415209cfcbca3978c0e0b9475f2c9e2067a079b6
김건우 <gun2935@naver.com>
2024-11-22 18:07:08 +0900

    feat : 참조에 의한 전달 활용 인수로 보낼 때에는 변수에 &를 붙이지 않는다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,35 +1,25 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-void change1(int x, int y);
-void change2(int &x, int &y);
+void gyesan(int& x, int& y);
 int main()
 {
 	int a, b;
-	printf("두 수를 입력하세요. ");
 	scanf("%d %d", &a, &b);
-	change1(a, b);
-	printf("첫 번째 함수 실행후 a = %d, b = %d\n", a,b);
-	change2(a, b);
-	printf("두 번째 함수 실행후 a = %d, b = %d\n", a,b);
-	
+	gyesan(a, b);
+
+	printf("%d %d", a, b);
 	return 0;
 }
-void change1(int x, int y)
+void gyesan(int& x, int& y)
 {
-	int tmp;
-	tmp = x;
-	x = y;
-	y = tmp;
-	
-	printf("첫 번째 함수 실행중 x = %d, y = %d\n", x, y);
-}
-void change2(int& x, int& y)
-{
-	int tmp;
-	tmp = x;
-	x = y;
-	y = tmp;
+	if (x > y) {
+		x=x / 2;
+		y=y * 2;
+	}
+	else {
+		x=x * 2;
+		y=y / 2;
+	}
 
-	printf("두 번째 함수 실행중 x = %d, y = %d\n", x, y);
 }
 
