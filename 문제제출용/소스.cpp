647cb9f0dde4b614ea1d50101cc0dcb6223ada30
김건우 <gun2935@naver.com>
2024-11-22 14:09:15 +0900

    feat : 함수 특정한 작업을 위해 독립적으로 만들어진 프로그램이다. 또한 함수의 내용이 기록된 것을 함수의 정의라고 한다. 표준라이브러리함수는 printf, scanf 와 같이 바로 사용이 가능, 사용자 정의 함수는 line()과 같이 사용자가 직접 정의하는 것이다. 리턴값이 없는 함수가 있는데, 이는 void로 정의한다. 코드의 line();은 함수의 호출이라고 하며, 실행순서가 호출된 함수로 이동하며, 그 내용을 실행하고, 다시 호출한 곳으로 돌아온다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,22 +1,15 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-
+void line()
+{
+	puts("==============================");
+}
 int main()
 {
-	int i, j;
-	char a[3][5];
-
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 5; j++) {
-			scanf(" %c", &a[i][j]);
-		}
-	}
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 5; j++) {
-			printf("%c ", a[i][j]+32);
-		}
-		printf("\n");
-	}
+	line();
+	puts("line 함수를 호출하였습니다.");
+	puts("line 함수를 다시 호출합니다.");
+	line();
 
 	return 0;
 }
