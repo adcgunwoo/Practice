4c0aa69c96b7c2652fbb570708f5c1cc4238214e
김건우 <gun2935@naver.com>
2024-11-23 11:45:49 +0900

    feat : 1부터 전달받은 수까지의 합 1부터 전달받은 값을 수까지의 합을 구하는 함수를 만들고, 메인에서 입력받은 값을 참조에 의한 전달로 보내고 호출해서 출력한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,33 +1,19 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-void input(int& x, int& y)
+int output(int &x)
 {
-	scanf("%d %d", &x, &y);
-}
-void change(int& x, int& y)
-{
-	int tmp = x;
-	x = y;
-	y = tmp;
-}
-void output(int& x, int& y)
-{
-	for (int i = x; i <= y; i++) {
-		printf("== %ddan ==\n", i);
-		for (int j = 1; j <= 9; j++) {
-			printf("%d * %d = %2d\n", i, j, i * j);
-		}
-		printf("\n");
+	int sum = 0;
+	for (int i = 1; i <= x; i++) {
+		sum += i;
 	}
+	return sum;
 }
-
 int main()
 {
-	int a, b;
-	input(a,b);
-	if (a > b) {
-		change(a, b);
-	}
-	output(a, b);
+	int a;
+	scanf("%d", &a);
+	int hap = output(a);
+	printf("%d", hap);
+
 	return 0;
 }
