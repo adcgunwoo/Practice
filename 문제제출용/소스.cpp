d0d9c41fa46ee7b20da922dd3233a52f8716039d
김건우 <gun2935@naver.com>
2024-11-24 14:35:35 +0900

    feat : const의 활용 변수를 선언할 때 앞에 const를 붙여주면, 변경할 수 없는 상수가 된다. 선언과 동시에 값이 주어져야 하며, 중간에 값을 변경시킨다면 컴파일에러가 발생한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,35 +2,13 @@
 #include <stdio.h>
 #include <math.h>
 //#include <stdlib.h>
-void input(double x[], int y)
-{
-	for (int i = 0; i < y; i++) {
-		scanf("%lf", &x[i]);
-	}
-}
-void sort(double q[], int w)
-{
-	for (int i = 0; i < w - 1; i++) {
-		for (int j = i + 1; j < w; j++) {
-			if (q[i] < q[j]) {
-				double tmp = q[i];
-				q[i] = q[j];
-				q[j] = tmp;
-			}
-		}
-	}
-}
-void output(double m[],int n)
-{
-	printf("%.0lf ", ceil(m[0]));
-	printf("%.0lf ", floor(m[2]));
-	printf("%.0lf ", round(m[1]));
-}
 int main()
 {
-	double a[3];	
-	input(a, 3);
-	sort(a, 3);
-	output(a, 3);
+	double ci,radius;
+	const double pi = 3.14;
+	scanf("%lf", &radius);
+	ci = 2 * radius * pi;
+	printf("%.2lf", ci);
+
 	return 0;
 }
