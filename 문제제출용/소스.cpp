44454c997da984ecfd0dfa7cbf5ce37c431ae677
김건우 <gun2935@naver.com>
2024-11-24 14:00:46 +0900

    feat : 반지름을 입력받아, 원의 넓이를 구하고 버림,반올림,올림을 이해하기 pow(a,2)는 a의 2승이고, floor(a)는 a의 소수점 버리기, round(a)는 소수점 반올림, ceil(a)는 소수점 올림이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -5,9 +5,15 @@
 
 int main()
 {
-	int a;
-	scanf("%d", &a);
-	printf("%.2lf", sqrt(a / 3.14));
+
+	double radius, area, pi = 3.14;
+	printf("원의 반지름 : ");
+	scanf("%lf", &radius);
+	area = pow(radius, 2) * pi;
+	printf("원의 넓이\n");
+	printf("버림 : %.0lf\n", floor(area));
+	printf("반올림 : %.0lf\n", round(area));
+	printf("올림 : %.0lf\n", ceil(area));
 
 	return 0;
 }
