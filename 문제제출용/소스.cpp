9940456da357b41d3afd23c759100c6b2cb22d96
김건우 <gun2935@naver.com>
2024-11-24 14:25:38 +0900

    feat : 선택정렬, 올림, 반올림, 내림 활용 in 함수 II 세 실수를 입력받아, 함수를 호출해 입력받고, 수를 내림차순으로 정렬하고, 올림,반올림, 내림을 각각 실행하고 출력했다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,18 +2,35 @@
 #include <stdio.h>
 #include <math.h>
 //#include <stdlib.h>
-
+void input(double x[], int y)
+{
+	for (int i = 0; i < y; i++) {
+		scanf("%lf", &x[i]);
+	}
+}
+void sort(double q[], int w)
+{
+	for (int i = 0; i < w - 1; i++) {
+		for (int j = i + 1; j < w; j++) {
+			if (q[i] < q[j]) {
+				double tmp = q[i];
+				q[i] = q[j];
+				q[j] = tmp;
+			}
+		}
+	}
+}
+void output(double m[],int n)
+{
+	printf("%.0lf ", ceil(m[0]));
+	printf("%.0lf ", floor(m[2]));
+	printf("%.0lf ", round(m[1]));
+}
 int main()
 {
-
-	double radius, area, pi = 3.14;
-	printf("원의 반지름 : ");
-	scanf("%lf", &radius);
-	area = pow(radius, 2) * pi;
-	printf("원의 넓이\n");
-	printf("버림 : %.0lf\n", floor(area));
-	printf("반올림 : %.0lf\n", round(area));
-	printf("올림 : %.0lf\n", ceil(area));
-
+	double a[3];	
+	input(a, 3);
+	sort(a, 3);
+	output(a, 3);
 	return 0;
 }
