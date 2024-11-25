5d10e58fe637a13c726355fdd0ea2e9c3fbababe
김건우 <gun2935@naver.com>
2024-11-25 17:39:34 +0900

    feat : 겹치지 않는 주사위 모든 경우의 수 현재의 스탭(주사위 순서에) 담을 값을 1부터가 아니라 이전 스텝에 담겨있는 값부터 시작. 첫번째 던져질 수를 1로 강제함

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,30 +1,29 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-//#include <math.h>
-int N, a[101]; //전역변수를 잘 사용하지 않지만, 인수로 주고받기 까다로운 문제이기에 선언
-void output()
-{
+
+int N, a[101];
+
+void output() {
 	for (int i = 1; i <= N; i++) {
-		printf("%d ", a[i]); 
+		printf("%d ", a[i]);
 	}
 	printf("\n");
 }
-void dice(int step)
-{
-	int i;
-	if (step > N) { // 던진 주사위가 입력된 값에 만족하면, 출력으로 넘어감.
+
+void dice(int step, int start) {
+	if (step > N) {
 		output();
-		return; //리턴
+		return;
 	}
-	for (i = 1; i <= 6; i++) {
-		a[step] = i; // 예를 들어, a[1]이면 첫번째 주사위를 던지는 것이다.
-		dice(step + 1); // 첫번째 주사위를 던졌으니, 그 다음 주사위를 던지는 것이다.
+
+	for (int i = start; i <= 6; i++) {  // start를 이용해 중복을 방지
+		a[step] = i;
+		dice(step + 1, i);  // 재귀 호출, i로 start를 설정하여 중복 방지
 	}
-}
-int main()
-{
-	scanf("%d", &N); //몇개의 주사위를 던질 것인지 입력
-	dice(1);
-	return 0;
 }
 
+int main() {
+	scanf("%d", &N);
+	dice(1, 1);  // 첫 번째 던지기, 1부터 시작
+	return 0;
+}
