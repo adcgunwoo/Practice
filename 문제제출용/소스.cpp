eb4a49f9517fe6c157fa11d21712b544574b8cb5
김건우 <gun2935@naver.com>
2024-11-25 17:53:48 +0900

    feat : 평범한 함수를 이용한 피보나치 수열 계산 재귀함수를 사용하지 않았음, 복습차원에서 작성

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,29 +1,21 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-
-int N, a[101];
-
-void output() {
-	for (int i = 1; i <= N; i++) {
-		printf("%d ", a[i]);
-	}
-	printf("\n");
-}
-
-void dice(int step, int start) {
-	if (step > N) {
-		output();
-		return;
-	}
-
-	for (int i = start; i <= 6; i++) {  // start를 이용해 중복을 방지
-		a[step] = i;
-		dice(step + 1, i);  // 재귀 호출, i로 start를 설정하여 중복 방지
+void sort(int x[], int y)
+{
+	for (int i = 2; i < y; i++) {
+		x[i] = x[i - 2] + x[i - 1];
 	}
 }
-
-int main() {
+void output(int x,int y[])
+{
+	printf("%d", y[x - 1]);
+}
+int main() 
+{
+	int N, a[51]={1,1,};
 	scanf("%d", &N);
-	dice(1, 1);  // 첫 번째 던지기, 1부터 시작
+	sort(a,51);
+	output(N,a);
+
 	return 0;
 }
