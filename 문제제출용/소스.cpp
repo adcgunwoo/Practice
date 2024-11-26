e61b503e015dc985c911693d38eee05104e09191
김건우 <gun2935@naver.com>
2024-11-26 16:15:24 +0900

    feat : string.h의 함수의 정의 문자열의 개수를 구하기 위해 새로운 헤더파일을 정의했다. 추가로 여러가지 문자열 함수들이 정의되 있다고 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,14 +1,15 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
+#include <string.h> //strlen함수를 사용하기 위해 정의
 int main()
 {
-	char arr[100]="jungol olympiad";
-	int a[5],i;
-	for (i = 0; i < 5; i++) {
-		scanf("%d", &a[i]);
-	}
-	for (i = 0; i < 5; i++) {
-		printf("%c", arr[a[i]]);
+	int len,i;
+	char a[100];
+	scanf("%s", a);
+	len = strlen(a); //문자의 개수
+	printf("입력받은 문자열의 길이는 %d입니다.\n",len);
+	for (i = len-1; i >=0; i--) {
+		printf("%c", a[i]);
 	}
 
 	return 0;
