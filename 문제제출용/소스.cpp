3ed57e67b4e8227f43c3276a976925916a7be208
김건우 <gun2935@naver.com>
2024-11-26 14:06:37 +0900

    feat : 재귀함수 형성평가 1 return 값과 호출할 함수의 값이 중요한데, 시간이 오래 걸려도 좋으니 곰곰히 잘 생각해보자.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,15 +1,17 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-int a[100] = { 0,1, };
-int sort(int x)
-{          //          sort로 해야 됨 a[x] 아니야!!! 
-	if (a[x]==0) a[x]= sort(x/2) +sort(x-1);
-	return a[x];
+void sort(int x)
+{
+	if (x == 0) {
+		return;	
+	}
+	sort(x / 2);
+	printf("%d ", x);
 }
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	printf("%d", sort(n));
+	int N;
+	scanf("%d", &N);
+	sort(N);
 	return 0;
 }
