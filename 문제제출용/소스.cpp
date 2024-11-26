9202837b2e6cb90eb43ce81893e00ec489ca0829
김건우 <gun2935@naver.com>
2024-11-26 18:03:51 +0900

    feat : fgets함수를 사용해 공백을 포함한 문장 입력받기 글이 길어져 주석처리로 대체하였고, stdin은 입력스트림(표준입력)을 의미한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,25 +1,18 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <ctype.h> // 문자type과 관련된 함수들이 정의되어있음.
-#include <string.h>
+#include <string.h> //fgets 함수를 사용하기 위해 정의
 
 int main()
 {
 	char a[100];
 	int len;
-
-	scanf("%s", a);
+	fgets(a, 100, stdin); //공백을 포함한 문장을 입력받기 위해 함수사용, scanf("%s",a)는 공백이 있으면 따로 인식
 	len = strlen(a);
 	for (int i = 0; i < len; i++) {
-		if (isupper(a[i])) {
-			a[i] += 32;
-		}
-		else if (islower(a[i])) {
-			a[i] -= 32;
-		}
-	}
-	for (int i = 0; i < len; i++) {
 		printf("%c", a[i]);
+		if (a[i] == ' ') { // " " 아님 절대 유의할것!
+			printf("\n");
+		}
 	}
 
 	return 0;
