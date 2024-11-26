be2c639ef09112b7eee2b4106b60ce3b20b8183d
김건우 <gun2935@naver.com>
2024-11-26 16:50:08 +0900

    feat : 컴파일러에 따라 다른 문장의 길이 출력 영어나 숫자는 한 글자가 1바이트이지만, 한글은 처리하는 방법에 따라 2바이트 또는 3바이트이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,19 +3,11 @@
 #include <string.h> //strlen함수를 사용하기 위해 정의
 int main()
 {
-	char a[100];
-	int b,len;
-
-	scanf("%s %d", a, &b);
-	len = strlen(a);
-	if (b >= len) {
-		for (int i = len - 1; i >= 0; i--) {
-			printf("%c", a[i]);
-		}
-	}
-	else for (int i = len - 1; i >= len-b; i--) {
-		printf("%c", a[i]);
-	}
+	int i = 0,len;
+	char str[50] = "우리나라 대한민국!";
+	len = strlen(str);
+	printf("%s\n", str);
+	printf("위 문자열의 길이는 %d입니다.", len);
 
 	return 0;
 }
