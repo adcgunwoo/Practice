cfccd78520ab143f182ad741470e7d7d48e2482a
김건우 <gun2935@naver.com>
2024-11-26 16:33:01 +0900

    feat : strlen 함수의 활용 입력받은 정수가 문자열의 길이보다 크다면, 모두 출력하고 그렇지 않다면 원래대로 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,12 +3,17 @@
 #include <string.h> //strlen함수를 사용하기 위해 정의
 int main()
 {
-	int len,i;
 	char a[100];
-	scanf("%s", a);
-	len = strlen(a); //문자의 개수
-	printf("입력받은 문자열의 길이는 %d입니다.\n",len);
-	for (i = len-1; i >=0; i--) {
+	int b,len;
+
+	scanf("%s %d", a, &b);
+	len = strlen(a);
+	if (b >= len) {
+		for (int i = len - 1; i >= 0; i--) {
+			printf("%c", a[i]);
+		}
+	}
+	else for (int i = len - 1; i >= len-b; i--) {
 		printf("%c", a[i]);
 	}
 
