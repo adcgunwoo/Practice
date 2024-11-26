a4a991ccf1e7319004926037c588c2809e24aa21
김건우 <gun2935@naver.com>
2024-11-26 17:13:14 +0900

    feat : ctype.h 헤더파일의 사용과 문자를 구별하기 위한 다양한 함수의 활용 길이가 길어질듯해 주석으로 설명을 대신함.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,13 +1,27 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <string.h> //strlen함수를 사용하기 위해 정의
+#include <ctype.h> // 문자type과 관련된 함수들이 정의되어있음.
+
 int main()
 {
-	int i = 0,len;
-	char str[50] = "우리나라 대한민국!";
-	len = strlen(str);
-	printf("%s\n", str);
-	printf("위 문자열의 길이는 %d입니다.", len);
+	char a;
+	while (1) {
+		a = getchar(); //문자 하나를 입력받아서, ch 변수에 저장
+		getchar(); // 위 식이 이뤄지면 입력받은 문자 + 엔터가 입력되는데, 엔터를 버려줌.
+		if (isupper(a)) { //대문자인지 확인
+			printf("대문자입니다.\n");
+		}
+		else if (islower(a)) { //소문자인지 확인
+			printf("소문자입니다.\n");
+		}
+		else if (isdigit(a)) { //숫자문자인지 확인
+			printf("숫자문자입니다.\n");
+		}
+		else {
+			printf("영문, 숫자 이외의 문자입니다."); // isalpha() 알파벳인지 확인, isalnum()숫자 또는 알파벳인지 확인 
+			break;
+		}
+	}
 
 	return 0;
 }
