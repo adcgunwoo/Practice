ca144b6e636e9c6d579cae01aef71a36bcce003f
김건우 <gun2935@naver.com>
2024-11-26 17:45:11 +0900

    feat : 대문자는 소문자로, 소문자는 대문자로 isupper 함수와 islower 함수를 활용해 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,26 +1,25 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <ctype.h> // 문자type과 관련된 함수들이 정의되어있음.
+#include <string.h>
 
 int main()
 {
-	char a;
-	while (1) {
-		a = getchar(); //문자 하나를 입력받아서, ch 변수에 저장
-		getchar(); // 위 식이 이뤄지면 입력받은 문자 + 엔터가 입력되는데, 엔터를 버려줌.
-		if (isupper(a)) { //대문자인지 확인
-			printf("대문자입니다.\n");
+	char a[100];
+	int len;
+
+	scanf("%s", a);
+	len = strlen(a);
+	for (int i = 0; i < len; i++) {
+		if (isupper(a[i])) {
+			a[i] += 32;
 		}
-		else if (islower(a)) { //소문자인지 확인
-			printf("소문자입니다.\n");
+		else if (islower(a[i])) {
+			a[i] -= 32;
 		}
-		else if (isdigit(a)) { //숫자문자인지 확인
-			printf("숫자문자입니다.\n");
-		}
-		else {
-			printf("영문, 숫자 이외의 문자입니다."); // isalpha() 알파벳인지 확인, isalnum()숫자 또는 알파벳인지 확인 
-			break;
-		}
+	}
+	for (int i = 0; i < len; i++) {
+		printf("%c", a[i]);
 	}
 
 	return 0;
