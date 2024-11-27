048f6c2dc5264be5615af61826d753687a1cdd1a
김건우 <gun2935@naver.com>
2024-11-27 18:37:28 +0900

    feat : strcpy(a,b) 및 중요한 문자열 함수의 활용 위 함수의 내용은 b를 a에 대입하는 것이다. strcat(a,b)는 a에 b를 연결하여 복사한다. strcmp(a,b)는 a와 b의 크기를 비교하고, strchr(a,b)는 a문자열에 b문자가 포함되어 있는지 확인하고, strstr(a,b)은 a문자열에 b문자열이 포함되어 있는지 확인하는 것이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,22 +3,19 @@
 #include <string.h>
 int main()
 {
-	int i, j,len[21];
-	char alpha, a[10][21] ={{0}};
-	
-	for (i = 0; i < 10; i++) {
-		scanf( "%s", a[i]); //%s 앞에 공백 웬만하면 두자
-		len[i] = strlen(a[i]);
-	}
-	scanf(" %c", &alpha); // %c 앞에 공백 둬야함!!
-	
-	for (i = 0; i < 10; i++) {
-		if (a[i][len[i] - 1] == alpha) {
-			printf("%s\n", a[i]);
-		}
-	}
+	char a[50], b[50],tmp[50];
+	int i, j,len1,len2;
 
+	scanf("%s %s", a, b);
+	len1 = strlen(a);
+	len2 = strlen(b);
 
+	if (len1 > len2) {
+		strcpy(tmp, a);
+		strcpy(a, b);
+		strcpy(b, tmp);
+	}
+	printf("%s\n%s", a, b);
 
 	return 0;
 }
