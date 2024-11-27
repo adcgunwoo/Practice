fb8887a28d9fa914a1b28ef13c7d9725e00be20c
김건우 <gun2935@naver.com>
2024-11-27 18:27:24 +0900

    feat : 문자열 끝 단어와 입력받은 문자가 같으면 출력 문장의 길이를 문자열을 입력받을때 같이 입력받고, 검사할때 자릿수를 나타내는 자리에 대입해서 출력한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,30 +3,22 @@
 #include <string.h>
 int main()
 {
-	char a[50][50],st[50];
-	int i,j=0,wcnt=0,wlen=0;
-
-	scanf(" %[^\r\n]", st); // scanf는 공백을 포함해서 입력받을 수 없다.
-	// %[^\n\r] 는 \n과 \r이 나오기 전까지 입력을 받으라는 것이다. %앞에 공백을 넣어야 함
-	int len = strlen(st); 
-
-	for (i = 0; i < len; i++) {
-		if (st[i] == ' ') {
-			a[wcnt][wlen] = '\0';
-			wcnt++;
-			wlen = 0;
-		}
-		else {
-			a[wcnt][wlen] = st[i];
-			wlen++;
+	int i, j,len[21];
+	char alpha, a[10][21] ={{0}};
+	
+	for (i = 0; i < 10; i++) {
+		scanf( "%s", a[i]); //%s 앞에 공백 웬만하면 두자
+		len[i] = strlen(a[i]);
+	}
+	scanf(" %c", &alpha); // %c 앞에 공백 둬야함!!
+	
+	for (i = 0; i < 10; i++) {
+		if (a[i][len[i] - 1] == alpha) {
+			printf("%s\n", a[i]);
 		}
 	}
-	a[wcnt][wlen] = '\0';
-	wcnt++;
 
-	for (i = 0; i < wcnt; i++) {
-		puts(a[i]);
-	}
+
 
 	return 0;
 }
