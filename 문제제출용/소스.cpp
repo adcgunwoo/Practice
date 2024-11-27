f15949120801852e6d3e1a2196760ab844e40797
김건우 <gun2935@naver.com>
2024-11-27 17:01:08 +0900

    feat : 이차원 문자 배열 문자배열은 마지막에 널값이 필요하므로 이제부터 20개를 입력받는다 하면, a[21] 이런식으로 범위를 하나를 더 선언해야 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,29 +1,17 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-int N;
 int main()
 {
-	char a[100];
-	int len,j,cnt=0,n;
-	scanf("%s", a);
-	len = strlen(a);
-	while(1) {
-		scanf("%d", &n);
-		if (n >= len) {
-			n = len;
-		}
-		for (j = 0; j < len; j++) {
-			if (n != j + 1) {
-				printf("%c", a[j]);
-				a[cnt++] = a[j];
-			}
-		}
-		len -= 1;
-		printf("\n");
-		cnt = 0;
-		if (len == 1) break;
+	char a[5][20];
+	int i,j;
+
+	for (i = 0; i < 5; i++) {
+		scanf("%s", a[i]);
 	}
+	for (i = 0; i < 5; i++) {
+		printf("%s\n", a[i]);
+	}
+
 	return 0;
 }
