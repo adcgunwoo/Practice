6c0022ca37f2ca63b44d6bf5ceab7405c535a543
김건우 <gun2935@naver.com>
2024-11-27 14:57:35 +0900

    feat : 문자열속 십진정수 문자열을 입력받고 이를 출력해야 할 때, 문자열속에 숫자가 있을 때 %d로 출력한다면 아스키코드값으로 출력되므로 %c로 문자 그대로 출력시키기

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,22 +1,25 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <string.h> //fgets 함수를 사용하기 위해 정의
-
+#include <ctype.h>
 int main()
 {
-	char a[100],tmp,pmt;
-	int len, i, j;
+	char a[100];
 	scanf("%s", a);
-	len = strlen(a);
-
+	int i,len = strlen(a);
+	
 	for (i = 0; i < len; i++) {
-		tmp = a[len - 1];
-		for (j=len-1; j>=0; j--){ // for (j=0; j<len-1; j++) { 왼쪽으로 돌렸을때와 차이
-			a[j] = a[j-1];	//            a[j] = a[j+1];         
+		if (isupper(a[i])) {
+			printf("%c", a[i] + 32);
 		}
-		a[0] = tmp;
-		printf("%s \n",a);
+		else if (islower(a[i])) {
+			printf("%c", a[i]);
+		}
+		else if (isdigit(a[i])) {
+			printf("%c", a[i]); //숫자라고 %d로 출력하면 안됨!
+		}
 	}
+
 
 	return 0;
 }
