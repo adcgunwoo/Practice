b609f97bb6f545e9b5324972235d5982d9348715
김건우 <gun2935@naver.com>
2024-11-27 17:53:14 +0900

    feat : 화이트스페이스(빈칸,탭,엔터)가 포함된 문자열 입력 scanf 에서 %[^\r\n] 를 사용하면 공백이 포함된 문장을 입력받을 수 있는데, 글이 길어져 주석으로 대신함.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,14 +3,29 @@
 #include <string.h>
 int main()
 {
-	char a[5][20];
-	int i,j;
+	char a[50][50],st[50];
+	int i,j=0,wcnt=0,wlen=0;
 
-	for (i = 0; i < 5; i++) {
-		scanf("%s", a[i]);
+	scanf(" %[^\r\n]", st); // scanf는 공백을 포함해서 입력받을 수 없다.
+	// %[^\n\r] 는 \n과 \r이 나오기 전까지 입력을 받으라는 것이다. %앞에 공백을 넣어야 함
+	int len = strlen(st); 
+
+	for (i = 0; i < len; i++) {
+		if (st[i] == ' ') {
+			a[wcnt][wlen] = '\0';
+			wcnt++;
+			wlen = 0;
+		}
+		else {
+			a[wcnt][wlen] = st[i];
+			wlen++;
+		}
 	}
-	for (i = 0; i < 5; i++) {
-		printf("%s\n", a[i]);
+	a[wcnt][wlen] = '\0';
+	wcnt++;
+
+	for (i = 0; i < wcnt; i++) {
+		puts(a[i]);
 	}
 
 	return 0;
