70f34b7a0105ec7e80e799e0a4e27bdb274889d9
김건우 <gun2935@naver.com>
2024-11-27 13:38:13 +0900

    feat : 문자열을 왼쪽으로 한 바퀴 회전하여 출력 각 행의 첫번째 값을 빼두고 현재의 값에 뒤에 오는 문자를 대입한다. 그 마지막 문자에 빼둔 값을 대입한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,16 +4,19 @@
 
 int main()
 {
-	char a[100];
-	int len;
-	fgets(a, 100, stdin); //공백을 포함한 문장을 입력받기 위해 함수사용, scanf("%s",a)는 공백이 있으면 따로 인식
+	char a[50],tmp;
+	int len,i,j;
+	scanf("%s", a); //fgets는 공백이 있는 문자열을 하나로 받는것이지 그냥 문자열은 안됨.
 	len = strlen(a);
-	for (int i = 0; i < len; i++) {
-		printf("%c", a[i]);
-		if (a[i] == ' ') { // " " 아님 절대 유의할것!
-			printf("\n");
+	for (i = 0; i < len; i++) {
+		tmp = a[0]; // a 배열의 값이 대입되면서 바뀔수있기에, 
+		for (j = 0; j < len-1; j++) {
+			a[j] = a[j+1];
 		}
+		a[len - 1] = tmp;
+		printf("%s \n", a);
 	}
+
 
 	return 0;
 }
