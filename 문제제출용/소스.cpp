ec073431307cbb92d83601e190efc271d08b4957
김건우 <gun2935@naver.com>
2024-11-28 13:54:21 +0900

    feat : strcat()의 활용과 주의점 strcat(a, b) 를 하면 a뒤에 b가 그대로 이어지는데 a의 배열은 b가 합쳐지는거 까지 고려해 글자수를 선언해야 한다. (주석참고)

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 int main()
-{
-	char a[50], b[50] = { "Hong Gil Dong" };
-	strcpy(a, b);
+{   // b를 a에 합쳐야 하기에 a는 배열의 크기를 22만큼 선언
+	char a[22], b[11], tmp[11]; 
+	scanf("%s %s", a,b);
 
+	strcpy(tmp, a);
+	strcpy(a, b);
+	strcpy(b, tmp);
+	strcat(a, b);
+						
 	printf("%s", a);
 
 	return 0;
