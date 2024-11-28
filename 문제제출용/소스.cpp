deb9f8a0b3711d80140953f1a7f584213747389c
김건우 <gun2935@naver.com>
2024-11-28 14:28:48 +0900

    feat : strncpy(a, b, n), strncat(a, b, n) 의 활용 strncpy(a,b,n) 는 a문자열 앞부터 b문자열 앞 n개의 문자를 복사하는 것이고, strncat(a, b, n) 은 a의 바로 뒤에 b문자의 앞부터 n개를 복사한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,16 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 int main()
-{   // b를 a에 합쳐야 하기에 a는 배열의 크기를 22만큼 선언
-	char a[22], b[11], tmp[11]; 
-	scanf("%s %s", a,b);
+{   
+	char a[20], b[20], c[40];
+	scanf("%s %s", a, b);
+	strcpy(c, a);
+	printf("%s\n",strncpy(c, b, 3));
+	printf("%s\n",strncat(b, a, 3));
 
-	strcpy(tmp, a);
-	strcpy(a, b);
-	strcpy(b, tmp);
-	strcat(a, b);
-						
-	printf("%s", a);
 
 	return 0;
 }
