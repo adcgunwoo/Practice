3822720df5206c1eb04fc7f00d7ca204441de8fe
김건우 <gun2935@naver.com>
2024-11-28 13:46:37 +0900

    feat : 배열에 문자열 저장 및 strcpy() 복습 배열에 문자열을 저장할 때 {" 문자열"} 이런 식으로 선언하고, strcpy(a, b) 이런 식으로 배열을 통째로 복사한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,19 +3,10 @@
 #include <string.h>
 int main()
 {
-	char a[50], b[50],tmp[50];
-	int i, j,len1,len2;
+	char a[50], b[50] = { "Hong Gil Dong" };
+	strcpy(a, b);
 
-	scanf("%s %s", a, b);
-	len1 = strlen(a);
-	len2 = strlen(b);
-
-	if (len1 > len2) {
-		strcpy(tmp, a);
-		strcpy(a, b);
-		strcpy(b, tmp);
-	}
-	printf("%s\n%s", a, b);
+	printf("%s", a);
 
 	return 0;
 }
