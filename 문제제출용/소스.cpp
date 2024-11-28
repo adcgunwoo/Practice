8f97fb7c923e2e6b9a99f5b921a1e819e0ba5deb
김건우 <gun2935@naver.com>
2024-11-28 17:40:15 +0900

    feat : atoi, atof 함수에서 유의해야할 점 문제처럼 50.1*34를 입력받을 때 atoi 함수를 실행하면 34를 실행할 수 있지만, '.' 앞에 50을 받는다. 주목해야할 점은 문자열의 처음부터 정수나 실수에 해당하는 부분을 변환하여 주는 것이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,13 +3,11 @@
 #include <stdlib.h> //atoi, atof 함수를 사용하기 위해 정의
 int main()
 {   
-	char a[11], b[11], n[11], m[11];
-	
-	scanf("%s %s", a, b);
-	scanf("%s %s", n, m);
-	
-	printf("%d + %d = %d\n", atoi(a), atoi(b), atoi(a) + atoi(b));
-	printf("%.2lf + %.2lf = %.2lf\n", atof(n), atof(m), atof(n) + atof(m));
+	char a[20];
+	scanf("%s", a);
+
+	printf("%d\n", atoi(a)*2);
+	printf("%.2lf\n", atof(a));
 
 	return 0;
 }
