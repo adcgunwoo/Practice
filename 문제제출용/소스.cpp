d830cb3226e851fe82a271b76af6d57e8262a5ad
김건우 <gun2935@naver.com>
2024-11-28 15:37:12 +0900

    feat : strchr, strstr 참, 거짓 확인 내 코드는 is, else문으로 만들어져서 작동하지만, 정수형 변수를 0으로 선언해 if문을 만족할시1로 대입, 값이 변하지 않고 0으로 나오면 "No"가 출력되도록 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,31 +3,21 @@
 #include <string.h>
 int main()
 {   
-	char n,m[20], a[10][20] = {"champion", "tel", "pencil", "jungol", "olympiad", "class", "information", "lesson", "book", "lion"};
-	int i, j, cnt = 0;;
-	printf("문자를 입력하세요. ");
-	scanf("%c", &n);
+	char a[100];
+	scanf("%s", a);
 
-	for (i = 0; i < 10; i++) {
-		if (strchr(a[i], n)) {
-			printf("%s\n", a[i]);
-			cnt += 1;
-		}
+	if (strchr(a, 'c')) {
+		printf("Yes ");
 	}
-	if (cnt == 0) {
-		printf("찾는 단어가 없습니다.\n");
+	else {
+		printf("No ");
 	}
-	cnt = 0;
-	printf("\n문자열을 입력하세요. ");
-	scanf("%s", m);
-	for (i = 0; i < 10; i++) {
-		if (strstr(a[i], m)) {
-			printf("%s\n", a[i]);
-			cnt += 1;
-		}
+
+	if (strstr(a, "ab")) {
+		printf("Yes ");
 	}
-	if (cnt == 0) {
-		printf("찾는 단어가 없습니다.\n");
+	else {
+		printf("No ");
 	}
 
 	return 0;
