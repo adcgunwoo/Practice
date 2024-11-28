a516667e7ea1c1eaecbd672fd9a75b4fb9573c27
김건우 <gun2935@naver.com>
2024-11-28 17:26:41 +0900

    feat : atoi(), atof() 함수 각 함수는 문자열의 처음부터 각각 정수,실수에 해당하는 부분을 정수, 실수로 변환하여 준다. 추가로 %lf를 떠올려 atolf()를 생각할 수 있는데 안된다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,27 +1,15 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h> //atoi, atof 함수를 사용하기 위해 정의
 int main()
 {   
-	char a[5][20],tmp[20];
-	int i,j;
-	printf("단어 5개를 입력하세요.\n");
-	for (i = 0; i < 5; i++) {
-		scanf("%s", a[i]);
-	}
-	for (i = 0; i < 4; i++) {
-		for (j = i + 1; j < 5; j++) {
-			if (strcmp(a[i], a[j]) > 0) {
-				strcpy(tmp, a[i]);
-				strcpy(a[i], a[j]);
-				strcpy(a[j], tmp);
-			}
-		}
-	}
-
-	for (i = 0; i < 5; i++) {
-		printf("%s\n", a[i]);
-	}
+	char a[11], b[11], n[11], m[11];
+	
+	scanf("%s %s", a, b);
+	scanf("%s %s", n, m);
+	
+	printf("%d + %d = %d\n", atoi(a), atoi(b), atoi(a) + atoi(b));
+	printf("%.2lf + %.2lf = %.2lf\n", atof(n), atof(m), atof(n) + atof(m));
 
 	return 0;
 }
