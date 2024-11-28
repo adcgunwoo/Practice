c65c3a12009d60f4723e232946cb49d98985c376
김건우 <gun2935@naver.com>
2024-11-28 18:03:38 +0900

    feat : sprintf()함수의 활용 글이 길어져 주석으로 처리했다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,13 +1,21 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <stdlib.h> //atoi, atof 함수를 사용하기 위해 정의
+//#include <stdlib.h> //atoi, atof 함수를 사용하기 위해 정의
+#include <string.h>
 int main()
 {   
-	char a[20];
-	scanf("%s", a);
-
-	printf("%d\n", atoi(a)*2);
-	printf("%.2lf\n", atof(a));
+	char arr[100];
+	double a, b, c;
+	scanf("%lf %lf %lf", &a, &b, &c);
+	sprintf(arr, "%.2lf%.2lf%.2lf", a, b, c); //세 변수의 내용을 문자열arr에 출력한다.
+	int i, len = strlen(arr);                 //출력한다는게 모니터에 출력하는 것이 아니라
+	                                          //지정된 문자열에 출력하여 저장하는 함수이다.
+	for (i = 0; i < len; i++) {               //문자뿐만 아니라 정수나 실수와 같은 자료형도
+		if (arr[i] == '.') {                  //일단 출력이 되면 모두 문자열로 저장한다.
+			printf("\n");
+		}
+		else printf("%c", arr[i]);
+	}
 
 	return 0;
 }
