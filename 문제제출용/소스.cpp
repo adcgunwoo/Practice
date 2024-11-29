65024266bf83b42304af1050cc6368a682931b55
김건우 <gun2935@naver.com>
2024-11-29 18:10:49 +0900

    feat : 구조체를 정의, 선언하는 또다른 방법 구조체를 정의할 때 자동으로 출력되는 ; 앞에 아무 단어를 추가하면, 정의와 변수 선언이 동시에 되는 것이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,18 +1,17 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-struct data //data 라는 이름의 새로운 구조체를 정의.
-{           //data는 구조체 태그라고 한다.
-	char name[20];  
-	int age;      //name과 age를 멤버변수라고 한다.
-}; // ; 있음
-int main()
-{  //c++에서는 그냥 data self; 로 선언할 수 있다.
-	struct data self;             //self라는 이름의 변수를 선언 
-	printf("당신의 이름은 무엇입니까? ");
-	scanf("%s", self.name);
-	printf("당신의 나이는 몇 살입니까? ");
-	scanf("%d", &self.age);
-	printf("당신의 이름은 %s이고 나이는 %d세이군요.", self.name, self.age);
-	
-	return 0; //예를 들어, 붕어빵 기계를 하나 만들었다고 하자. 
-}             //가계의 이름을 data라고 한다면, name과 age는 팥과 밀가루라고 할 수 있다.
+struct data
+{
+	char name[10];
+	int age;
+}buddy; //구조체 정의와 동시에 변수를 선언하는 방법으로 
+int main() // struct data buddy; 와 같다.
+{                                    
+	                                //구조체형 변수 self를 선언하고 이름과 나이를 초기화
+	struct data self={"손예준",12}; //구조체에 정의된 순서대로 멤버변수에 대입된다.
+	printf("친한 친구의 이름과 나이를 입력하세요. ");
+	scanf("%s %d", &buddy.name, &buddy.age);
+	printf("당신의 이름 : %s, 나이 : %d\n", self.name, self.age);
+	printf("친구의 이름 : %s, 나이 : %d", buddy.name, buddy.age);
+	return 0;
+}
