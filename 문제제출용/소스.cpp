dcb2847670d1802995342c112a2846786a98f9e9
김건우 <gun2935@naver.com>
2024-11-29 17:35:10 +0900

    feat : 구조체 서로 다른 자료형의 자료를 처리하기 위해 정의하는 것이다. 글이 길어질 듯해 주석으로 처리함.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,44 +1,18 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <string.h>
+struct data //data 라는 이름의 새로운 구조체를 정의.
+{           //data는 구조체 태그라고 한다.
+	char name[20];  
+	int age;      //name과 age를 멤버변수라고 한다.
+}; // ; 있음
 int main()
-{   
-	int i,j,a,len,cnt=0,wcnt=0;
-	double b;
-	char arr[100],c[30],half[2][50];
-
-	scanf("%d %lf %s",&a, &b, c);
-	sprintf(arr, "%d%.3lf%s", a, b, c); //arr에 세 자료형을 합침
-	len = strlen(arr);
-
-	if (len % 2 == 0) { //길이가 짝수일 때
-		for (i = 0; i < len; i++) {
-			if (i == len / 2) { //절반에 도착했을때
-				printf("\n"); //엔터, 줄바꾸기
-				half[cnt][wcnt] = '\0'; //마지막에 널값을 넣어주며, 쓰레기값이 같이 나오는거 차단
-				cnt++; // 두번째 단어를 출력하기위한 증가
-				wcnt = 0; //다음 값을 입력하기 위해 0으로 초기화
-			}
-			half[cnt][wcnt++] = arr[i]; //입력받기
-		}
-	}
-	else { //길이가 홀수일때
-		for (i = 0; i < len; i++) {
-			if (i == (len / 2) + 1) {
-				printf("\n");
-				half[cnt][wcnt] = '\0'; //널값 넣어주기, 이걸 실행하지 않으면,뒤에 쓰레기값도 같이 나옴
-				cnt++; // 행 바꿔주기   
-				wcnt = 0; //초기화
-			}
-			half[cnt][wcnt++] = arr[i];
-		}
-	}
-	half[cnt][wcnt] = '\0'; 
-	cnt++;
-
-	for (i = 0; i < cnt; i++) {
-		printf("%s\n", half[i]);
-	}
-
-	return 0;
-}
+{  //c++에서는 그냥 data self; 로 선언할 수 있다.
+	struct data self;             //self라는 이름의 변수를 선언 
+	printf("당신의 이름은 무엇입니까? ");
+	scanf("%s", self.name);
+	printf("당신의 나이는 몇 살입니까? ");
+	scanf("%d", &self.age);
+	printf("당신의 이름은 %s이고 나이는 %d세이군요.", self.name, self.age);
+	
+	return 0; //예를 들어, 붕어빵 기계를 하나 만들었다고 하자. 
+}             //가계의 이름을 data라고 한다면, name과 age는 팥과 밀가루라고 할 수 있다.
