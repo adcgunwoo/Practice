02516933e7efb9ea4a6a953711c8bed138d14c4a
김건우 <gun2935@naver.com>
2024-11-29 15:22:01 +0900

    feat : 문자열을 비교하고, 아스키코드 오름차순으로 출력(형성평가 4) 비교및 복사함수를 형성평가 문제를 풀면서 복습했다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,18 +3,23 @@
 #include <string.h>
 int main()
 {   
-	char a[50][101],alpha='0';
-	int cnt = 0,wcnt=0,i;
-	for (i = 0; i < 50; i++) {
+	int n,i,j;
+	char a[11][101],tmp[101];
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
 		scanf("%s", a[i]);
-		wcnt++;
-		if (strchr(a[i],alpha)) { // if (a[i][0]==alpha) 의 조건문으로도 작성할 수 있다.
-			break;
-		}
-		cnt++;
 	}
-	printf("%d\n", cnt);
-	for (i = 0; i < cnt; i+=2) {
+	for (i = 0; i < n-1; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (strcmp(a[i], a[j]) > 0) {
+				strcpy(tmp, a[i]);
+				strcpy(a[i],a[j]);
+				strcpy(a[j], tmp);
+			}
+		}
+	}
+	for (i = 0; i < n; i++) {
 		printf("%s\n", a[i]);
 	}
 
