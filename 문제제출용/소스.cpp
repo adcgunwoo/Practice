770c0d1e378cf943141295ec2c80f52d80364c00
김건우 <gun2935@naver.com>
2024-11-30 14:35:27 +0900

    feat : 두개 이상의 구조체 변수 선언과 단어 복습 글이 길어져 주석처리함

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,17 +1,20 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-struct data
+struct data //data 는 구조체 태그라고 한다
 {
-	char name[10];
-	int age;
-}buddy; //구조체 정의와 동시에 변수를 선언하는 방법으로 
-int main() // struct data buddy; 와 같다.
+	char name[20]; // 구조체에 정의된 이 세개는  
+	int kor,eng;   // 멤버변수라고 한다.
+};  //요기에 선언할 수 있다는 것도 기억해야함
+int main() 
 {                                    
-	                                //구조체형 변수 self를 선언하고 이름과 나이를 초기화
-	struct data self={"손예준",12}; //구조체에 정의된 순서대로 멤버변수에 대입된다.
-	printf("친한 친구의 이름과 나이를 입력하세요. ");
-	scanf("%s %d", &buddy.name, &buddy.age);
-	printf("당신의 이름 : %s, 나이 : %d\n", self.name, self.age);
-	printf("친구의 이름 : %s, 나이 : %d", buddy.name, buddy.age);
+	struct data lee,jun; // 두 개의 변수를 이렇게 선언할 수 있다.
+	
+	scanf("%s %d %d", lee.name, &lee.kor, &lee.eng);
+	scanf("%s %d %d", jun.name, &jun.kor, &jun.eng);
+
+	printf("%s %d %d\n", lee.name, lee.kor, lee.eng);
+	printf("%s %d %d\n", jun.name, jun.kor, jun.eng);
+	printf("합계 %d %d", lee.kor + jun.kor, lee.eng + jun.eng);
+	
 	return 0;
 }
