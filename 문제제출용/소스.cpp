b2fcdaa9e6e96de13c75bd012c59aac78b771b1f
김건우 <gun2935@naver.com>
2024-12-01 14:16:09 +0900

    feat : 이름순으로의 의미(아스키코드) 이름순으로는 말 그대로 가나다로 생각했으나, 아스키코드값 오름차순으로 이해하면 되겠다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,47 +3,80 @@
 #include <string.h>
 struct data
 {
-	int height;
-	double weight;
-	char name[20]; //이름이 3글자이기에 name[4]정도로 했지만, 런타임에러가 떴음
+	int h;
+	double w;
+	char name[20];
 }a[5],tmp;
-void input(struct data n[], int x)
+void input(struct data n[], int m)
 {
 	int i;
-	for (i = 0; i < x; i++) {
-		scanf("%s %d %lf", n[i].name, &n[i].height, &n[i].weight);
+
+	for (i = 0; i < m; i++) {
+		scanf("%s %d %lf", n[i].name, &n[i].h, &n[i].w);
 	}
 }
-void sort(struct data n[], int x)
+void sort_n(struct data n[], int m)
 {
 	int i,j;
-	for (i = 0; i < x - 1; i++) {
-		for (j = i + 1; j < x; j++) {
-			if (n[i].height > n[j].height) {
+	for (i = 0; i < m-1; i++) {
+		for (j = i + 1; j < m; j++) {
+			if (strcmp(n[i].name,n[j].name)>0) { //n[i].name이 아스키코드값으로 더 크면..
 				strcpy(tmp.name, n[i].name);
 				strcpy(n[i].name, n[j].name);
 				strcpy(n[j].name, tmp.name);
-				tmp.height = n[i].height;
-				n[i].height = n[j].height;
-				n[j].height = tmp.height;
-				tmp.weight = n[i].weight;
-				n[i].weight = n[j].weight;
-				n[j].weight = tmp.weight;
+				tmp.h = n[i].h;
+				n[i].h = n[j].h;
+				n[j].h = tmp.h;
+				tmp.w = n[i].w;
+				n[i].w = n[j].w;
+				n[j].w = tmp.w;
 			}
 		}
 	}
 }
-void output(struct data n[], int x)
+void output_n(struct data n[], int m)
+{
+	int i = 0;
+	printf("name\n");
+	for (i = 0; i < m; i++) {
+		printf("%s %d %.1lf\n", n[i].name,n[i].h, n[i].w);
+	}
+	printf("\n");
+}
+void sort_w(struct data n[], int m)
+{
+	int i, j;
+	for (i = 0; i < m - 1; i++) {
+		for (j = i + 1; j < m; j++) {
+			if (n[i].w < n[j].w) {
+				strcpy(tmp.name, n[i].name);
+				strcpy(n[i].name, n[j].name);
+				strcpy(n[j].name, tmp.name);
+				tmp.h = n[i].h;
+				n[i].h = n[j].h;
+				n[j].h = tmp.h;
+				tmp.w = n[i].w;
+				n[i].w = n[j].w;
+				n[j].w = tmp.w;
+			}
+		}
+	}
+}
+void output_w(struct data n[], int m)
 {
 	int i;
-	for (i = 0; i < x; i++) {
-		printf("%s %d %.1lf\n", n[i].name, n[i].height, n[i].weight);
+	printf("weight\n");
+	for (i = 0; i < m; i++) {
+		printf("%s %d %.1lf\n", n[i].name, n[i].h, n[i].w);
 	}
 }
 int main()
 {
 	input(a, 5);
-	sort(a, 5);
-	output(a, 5);
+	sort_n(a, 5);
+	output_n(a, 5);
+	sort_w(a, 5);
+	output_w(a, 5);
+
 	return 0;
 }
