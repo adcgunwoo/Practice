72cce9fdb18d8379c5b64ba05a484ce2df07b04b
김건우 <gun2935@naver.com>
2024-12-01 13:40:17 +0900

    feat : 런타임에러 출력도 잘 나오는데, 채점하면 런타임에러가 떠서 당황했다. 해결방법은 name의 배열 크기를 늘려주었더니, 해결되었다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -4,29 +4,46 @@
 struct data
 {
 	int height;
-	char name[20];
-}a[5],small;
-struct data cal(struct data arr[], int n)
+	double weight;
+	char name[20]; //이름이 3글자이기에 name[4]정도로 했지만, 런타임에러가 떴음
+}a[5],tmp;
+void input(struct data n[], int x)
 {
 	int i;
-	small.height = a[0].height;
-	strcpy(small.name, a[0].name);
-	for (i = 0; i < n; i++) {
-		if (small.height > a[i].height) {
-			small.height = a[i].height;
-			strcpy(small.name, a[i].name);
+	for (i = 0; i < x; i++) {
+		scanf("%s %d %lf", n[i].name, &n[i].height, &n[i].weight);
+	}
+}
+void sort(struct data n[], int x)
+{
+	int i,j;
+	for (i = 0; i < x - 1; i++) {
+		for (j = i + 1; j < x; j++) {
+			if (n[i].height > n[j].height) {
+				strcpy(tmp.name, n[i].name);
+				strcpy(n[i].name, n[j].name);
+				strcpy(n[j].name, tmp.name);
+				tmp.height = n[i].height;
+				n[i].height = n[j].height;
+				n[j].height = tmp.height;
+				tmp.weight = n[i].weight;
+				n[i].weight = n[j].weight;
+				n[j].weight = tmp.weight;
+			}
 		}
 	}
-	return small;
+}
+void output(struct data n[], int x)
+{
+	int i;
+	for (i = 0; i < x; i++) {
+		printf("%s %d %.1lf\n", n[i].name, n[i].height, n[i].weight);
+	}
 }
 int main()
 {
-	int i;
-	for (i = 0; i < 5; i++) {
-		scanf("%s %d", a[i].name, &a[i].height);
-	}
-	small = cal(a, 5);
-	printf("%s %d", small.name, small.height);
-
+	input(a, 5);
+	sort(a, 5);
+	output(a, 5);
 	return 0;
 }
