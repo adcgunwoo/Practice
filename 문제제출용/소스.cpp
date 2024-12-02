1e6a3c628a125047f4edc9f0fd5984ba7e09b55a
김건우 <gun2935@naver.com>
2024-12-02 15:30:12 +0900

    feat : 포인터에서 new int의 활용(in C++)

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,19 +2,14 @@
 #include <stdio.h>
 int main()
 {
-	int a;
-	int* p = &a;
-	scanf("%d", &a); 
-	printf("a = %d, *p = %d\n", a, *p);
-	
-	scanf("%d", p); // 포인터는 &없이 입력 가능
-	printf("a = %d, *p = %d\n", a, *p);
-	*p = *p + 5; //계산도 가능
-	printf("a = %d, *p = %d\n", a,*p);
-	a = a + 15;
-	printf("a = %d, *p = %d\n", a, *p);
-	*p = *p - 1;
-	printf("a = %d, *p = %d\n", a, *p);
+	int* a = new int; //일반적으로 자료를 저장하기 위해 
+	int* b = new int; //메모리 공간을 확보하려면 변수를 선언해야하는데
+	// new int는 변수 이름이 없이 정수형 메모리 공간을 확보하려는 것이다.
+	scanf("%d %d", a, b);
+	printf("%d + %d = %d", *a, *b, *a + *b);
 
+	delete a; //5,6번째 줄에 할당된 메모리 제거
+	delete b; // new 키워드로 생성된 동적메모리는 함수가 끝나도 종료되지 않음
+	//그러면 계속 메모리를 점유하게 되므로, 함수를 끝내기전 종료시키는 것이 바람직
 	return 0;
 }
