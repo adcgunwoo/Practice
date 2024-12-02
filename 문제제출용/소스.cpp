2b9c8ffcbf68228e88b4b29150302a81c3cb8a03
김건우 <gun2935@naver.com>
2024-12-02 16:18:27 +0900

    feat : 포인터를 배열에서 사용하는 법 포인터 변수에 배열을 저장했을 떄, 출력하는 방법은 a[i]과 *(p+i)와 같다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 int main()
 {
-	int* a = (int*)malloc(sizeof(int)); //일반적으로 자료를 저장하기 위해 
-	int* b = (int*)malloc(sizeof(int)); //메모리 공간을 확보하려면 변수를 선언해야함
-	// sizeof() 자료형의 byte수를 반환, int는 4, double은 8,char 1과 같음
-	scanf("%d %d", a, b);
-	printf("%d + %d = %d", *a, *b, *a + *b);
+	int i,a[5];
+	int* p = a;
+	
+	for (i = 0; i < 5; i++) {
+		scanf("%d", p+i);
+	}
+	for (i = 0; i < 5; i+=2) {
+		printf("%d ", *(p+i)); //포인터에서 배열을 표시하는 방식
+	}
+	/*
+	for (i=0; i<5; i+=2){
+		printf("%d ",a[i]); //일반적인 배열 출력
+	}
+	*/
 
-	free(a); //5,6번째 줄에 할당된 메모리 제거
-	free(b); // malloc 키워드로 생성된 동적메모리는 함수가 끝나도 종료되지 않음
-	//그러면 계속 메모리를 점유하게 되므로, 함수를 끝내기전 종료시키는 것이 바람직
 	return 0;
 }
