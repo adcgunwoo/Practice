8a89feee286a8d439988bed8170686b0e7be0d93
김건우 <gun2935@naver.com>
2024-12-02 17:50:16 +0900

    feat : 주소로 전달해서 포인터 변수로 받기, 배열을 전달해서 포인터 변수로 받기 글이 길어 질듯해 주석으로 대체

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,21 +1,43 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <stdlib.h>
+void input(int &x, int &y)
+{
+	scanf("%d %d", &x, &y);
+}
+void sort1(int *x, int *y) //매개변수를 포인터로 선언해 주소를 전달받아 저장한다.
+{                          //포인터는 주소만 저장할 수 있으므로 이 함수를 호출하기 위해
+	int tmp;               //반드시 주소를 전달해야하며 일반적인 값을 전달할 수 없다.
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+void sort2(int *q, int w)  //배열을 전달할 때, 배열로 받으면 상관없으나,
+{                          //포인터 변수로 받으면 뒤에 []를 작성하면 안됨.
+	int i, j;
+	for (i = 0; i < w - 1; i++) {
+		for (j = i + 1; j < w; j++) {
+			if (q[i] > q[j]) {
+				int tmp = q[i];
+				q[i] = q[j];
+				q[j] = tmp;
+			}
+		}
+	}
+}
+void output(int n, int m, int z[], int h)
+{
+	int i;
+	printf("%d %d\n", n, m);
+	for (i = 0; i < h; i++) {
+		printf("%d ", z[i]);
+	}
+}
 int main()
 {
-	int N,i;
-	int* p;
-	/*scanf("%d",&N);
-	*int a[N];    이 주석처리된 문장들은 a의 개수를 알수가 없으므로,실행시 에러가 뜸
-	*/ 
-	scanf("%d", &N);
-	p = new int[N];
-	for (i = 0; i < N; i++) {
-		scanf("%d", p + i);
-	}
-	for (i = N - 1; i >= 0; i--) {
-		printf("%d ", *(p + i));
-	}
-	delete []p; //동적배열을 선언했기에 지워줘야함
-	return 0;   //[]이 포인터 변수 앞에 선언해야함
+	int a, b,arr[5]={1,3,9,4,2};
+	input(a, b);
+	sort1(&a, &b); //포인터는 주소로만 저장할 수 있으므로, 주소로 전달
+	sort2(arr, 5); //매개변수를 받는 곳은 포인터 변수로 받았는데, 인수를 전달하는 곳은 평범함
+	output(a, b, arr, 5);
+	return 0;
 }
