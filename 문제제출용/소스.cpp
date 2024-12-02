ae6d7e15c44c5a82abebbd4f55a7146d98b37876
김건우 <gun2935@naver.com>
2024-12-02 14:50:13 +0900

    feat : 포인터 주로 뜨는 에러 포인터형 정수를 선언했는데 또 *p=&a 라든지, p=a 처럼 한 쪽은 포인터나 주소를 가르킨다면 에러가 뜬다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,12 +3,10 @@
 int main()
 {
 	int a = 10;
-	int* p=&a;
+	int* p;
+	scanf("%d", &a);
+	p = &a;
+	printf("%#p %d", p,*p);
 
-	printf("a = %d, &a = %p\n", a, &a); //%p는 주소를 16진수로 표시하는 서식문자다.
-	printf("*p = %d, p = %p\n", *p, p); //만약 0xxxx의 형태로 출력하려면 %#p로 작성하자.
-	//p에는 a의 주소가 저장되어 있으므로
-	// *p를 출력하면 p에 저장된 주소가
-	//가리기는 위치에 있는 값 즉,a가 출력된다.
 	return 0;
 }
