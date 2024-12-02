43e3988eb5d150a94c111446a7bc2f6e6a79c2a4
김건우 <gun2935@naver.com>
2024-12-02 13:52:39 +0900

    feat : 구조체형을 리턴하는 함수의 활용 in 형성평가 4 구조체 단원을 마무리하면서 구조체형을 리턴하는 함수를 작성했다. 또한 헤더파일 math.h를 정의하고 소수부분을 버리는 함수를 실행했다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,48 +1,31 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
+#include <math.h>
 struct data
 {
-	int x[2], y[2];
-}f[2],s[2],result_f[2],result_s[2];
-void input(struct data n[],struct data m[])
+	double hei, wei;
+}a,b,result;
+void input(struct data &x, struct data &y)
 {
-	int i;
-	for (i = 0; i < 2; i++) {
-		scanf("%d %d %d %d", &n[i].x[i], &n[i].y[i], &m[i].x[i], &m[i].y[i]);
-	}
+	scanf("%lf %lf", &x.hei, &x.wei);
+	scanf("%lf %lf", &y.hei, &y.wei);
 }
-void sort(struct data q[],struct data w[])
+struct data sort(struct data& q, struct data& w)
 {
-	int i, j;
-	for (i = 0; i < 1; i++) { //검사, 다른 답들과 비교하기
-		for (j = i + 1; j < 2; j++) {
-			if (q[i].x[i] > q[j].x[j]) {
-				result_f[i].x[i] = q[j].x[j];
-			}
-			else result_f[i].x[i] = q[i].x[i];
-			if (q[i].y[i] < q[j].y[j]) {
-				result_f[i].y[i] = q[i].y[i];
-			}
-			else result_f[i].y[i] = q[j].y[j];
-			if (w[i].x[i] < w[j].x[j]) {
-				result_s[i].x[i] = w[j].x[j];
-			}
-			else result_s[i].x[i] = w[i].x[i];
-			if (w[i].y[i] > w[j].y[j]) {
-				result_s[i].y[i] = w[i].y[i];
-			}
-			else result_s[i].y[i] = w[j].y[j];
-		}
-	}
+	result.hei = (q.hei + w.hei) / 2 + 5;
+	result.wei = (q.wei + w.wei) / 2 - 4.5;
+	return result;
 }
-void output(struct data h[], struct data j[])
+void output(struct data& o)
 {
-	printf("%d %d %d %d", result_f[0].x[0], result_f[0].y[0], result_s[0].x[0], result_s[0].y[0]);
+	printf("height : %.0lfcm\n", floor(result.hei));
+	printf("weight : %.1lfkg\n", result.wei);
 }
 int main()
 {
-	input(f, s);
-	sort(f, s);
-	output(f, s);
+	input(a, b);
+	result=sort(a, b);
+	output(result);
+
 	return 0;
 }
