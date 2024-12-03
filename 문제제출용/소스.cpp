63792d2798ee186b4e2add749c9deb508a03c394
김건우 <gun2935@naver.com>
2024-12-03 16:25:22 +0900

    feat : 파일입출력 입력과 출력을 위해서는 스트림이 필요한데, 키보드로 입력을하면 "stdin"이라는 표준입력 스트림을 이용하여 프로그램에 자료가 전달이 된다. 하지만 키보드와 모니터가 아닌 파일로 입력과 출력을 받으려면, 사용자가 스트림을 직접 정의해야한다. 자세한건 주석으로.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,28 +2,25 @@
 #include <stdio.h>
 int main()
 {
-	int n,i,max,min;
-	int* p = &n;
-	scanf("%d", p);
-	int *q = new int[n];
+	int sum, kor, eng;
+	double avg;
+	char name[100];
+    //아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
+	FILE *in, *out;  //포인터 변수의 이름은 곧 스트림의 이름이 된다.
+	in = fopen("input.txt", "r"); //입력 파일스트림을 생성, 
+	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
+	out = fopen("output.txt", "w"); //출력 파일 스트림을 생성 
+	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결
+	
+	fscanf(in, "%s %d %d", name, &kor, &eng);//in과 연결된 파일로부터 자료를 입력받음
+	sum = kor + eng;
+	avg = (kor + eng) / 2.0;
 
-	for (i = 0; i < *p; i++) {
-		scanf("%d", q + i);	
-	}
-	max = *q;
-	min = *q;
-	for (i = 0; i < *p; i++) {
-		if (max < *(q + i)) {
-			max = *(q + i);
-		}
-		else if (min > *(q + i)) {
-			min = *(q + i);
-		}
-	}
-	printf("max : %d\n", max);
-	printf("min : %d\n", min);
-
-	delete []q;
+	printf("%s %d %.1lf\n", name, sum, avg);
+	fprintf(out, "%s %d %.1lf\n", name, sum, avg); //입력과 방법이 같음,똑같이 출력
+	
+	fclose(in); //함수가 종료될때 닫히지만
+	fclose(out); //예기치 않게 중단될때를 대비해 작성해야함.
 
 	return 0;
 }
