7e856ca23ce963fe5aaf3def6c0ea492bf94feba
김건우 <gun2935@naver.com>
2024-12-03 17:09:45 +0900

    feat : 상대경로, 절대경력 in 파일입출력 현재 위치를 기준으로 어떤 폴더의 위치를 나타내는 것을 상대경로라 한다. 반면에 현재 위치와 관계없이 컴퓨터 내에서 어떤 폴더의 위치를 나타내는 것을 절대경로라 한다. 예를 들어 전체 경로가 /home/com/1802 라고 할때,현재 /home/com 이라고 하면 1802를 상대 주소로 표시하면 ./1802이고, 절대경로로는 /home/com/1802 이다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -2,22 +2,25 @@
 #include <stdio.h>
 int main()
 {
-	int a, b,result=0,i;
+	int a[10],i;
     //아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	FILE *in, *out;  //포인터 변수의 이름은 곧 스트림의 이름이 된다.
-	in = fopen("input.txt", "r"); //입력 파일스트림을 생성, 
+	in = fopen("./temp/input.txt", "r"); //입력 파일스트림을 생성, 
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
-	out = fopen("output.txt", "w"); //출력 파일 스트림을 생성 
+	out = fopen("./temp/output.txt", "w"); //출력 파일 스트림을 생성 
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결
 	
-	fscanf(in, "%d %d", &a,&b);//in과 연결된 파일로부터 자료를 입력받음
-	for (i = a; i <= b; i++) {
-		result+=i;
+	
+	for (i = 0; i < 10; i++) {
+		fscanf(in, "%d", &a[i]);//in과 연결된 파일로부터 자료를 입력받음
 	}
 
-	printf("%d\n", result);
-	fprintf(out, "%d", result); //입력과 방법이 같음,똑같이 출력
-	
+	for (i = 1; i < 10; i+=2) {
+		printf("%d ", a[i]);
+	}
+	for (i = 1; i < 10; i+=2) { //입력과 방법이 같음,똑같이 출력
+		fprintf(out, "%d ", a[i]);
+	}
 	fclose(in); //함수가 종료될때 닫히지만
 	fclose(out); //예기치 않게 중단될때를 대비해 작성해야함.
 
