208d0b822bdeae7bfd65e23050b7eb7729c201d9
김건우 <gun2935@naver.com>
2024-12-04 16:43:35 +0900

    feat : fscanf 와 fgets를 함께 쓸 때 주의점 주석 참고.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,32 +1,37 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <string.h>
+struct data
+{
+	int age;
+	char name[20],arr[200];
+}tmp[10];
 int main()
 {
-	int i, j;
-	char a[101];
+	int i,n;
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
 	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	fgets(a, 101, in); //in 자리에 창에서 입력받으면 stdin이지만, 파일에서 입력을 받으므로
-	                   //in이 그 자리에 들어간다.
-	int len = strlen(a);
-	while (a[len - 1] == '\r' || a[len - 1] == '\n') { 
-		a[--len] = '\0'; //엔터등 행바꿈 문자가 있으면 널값으로 바꿔주고, 길이를 한개 줄여준다.
-	}
 	
-	for (i = 0; i < len; i++) {
-		if (a[i] == ' ') {
-			fprintf(out, "\n");
-		}
-		else {
-			fprintf(out, "%c", a[i]);
-		}
+	fscanf(in, "%d", &n);
+	for (i = 0; i < n; i++) {
+		fscanf(in, "%s %d\n", tmp[i].name, &tmp[i].age); //age를 입력받으면 15가 저장되고 커서는 15 다음에 위치하게 된다.
+		fgets(tmp[i].arr, 200, in);                      //다음에 fgets를 실행하면 커서가 위치한 곳으로부터 
+	}                                                    //그 행의 마지막까지 입력받게 되는데 그 행의 뒤는 아무것도 없기에 입력을 못받는다.
+	for (i = 0; i < n; i++) {                            //따라서 다음 행의 내용을 입력받으려면 커서의 위치를 다음행으로 이동시키기 위해
+		fprintf(out, "이름 : %s\n", tmp[i].name);        //fscanf 내에 \n을 붙여준다.
+		fprintf(out, "나이 : %d세\n", tmp[i].age);
+		fprintf(out, "수상실적 : %s\n", tmp[i].arr);
 	}
-	
+	for (i = 0; i < n; i++) {
+		printf("이름 : %s\n", tmp[i].name);
+		printf("나이 : %d세\n", tmp[i].age);
+		printf("수상실적 : %s\n", tmp[i].arr);
+	}
+
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	
