edf9c8c8384d3620c1a08c16a76c1d8c74f0d194
김건우 <gun2935@naver.com>
2024-12-04 15:07:05 +0900

    feat : 파일 입출력으로 성적표 출력 II 이전 문제와 다른점은 성적순, 순위순으로 출력하지 않는다는 점이다. 따라서 복사본으로 점수를 내림차순으로 정렬하고, 등수를 매기고, 다시 원본에 다중 반복으로 순위를 대입시켰다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -6,26 +6,36 @@ struct data
 {
 	char name[20];
 	int score, rank, cnt = 0;
-}a[100], tmp;
+}a[100], cpy[100],tmp;
 void sort()
 {
 	int i, j;
+	for (i = 0; i < N; i++) {
+		cpy[i] = a[i];
+	}
 	for (i = 0; i < N - 1; i++) { //성적 내림차순으로 정렬하기위해 선택정렬
 		for (j = i + 1; j < N; j++) {
-			if (a[i].score < a[j].score) {
-				tmp = a[i];
-				a[i] = a[j];
-				a[j] = tmp;
+			if (cpy[i].score < cpy[j].score) {
+				tmp = cpy[i];
+				cpy[i] = cpy[j];
+				cpy[j] = tmp;
 			}
 		}
 	}
-	a[0].rank = 1; //내림차순으로 정렬된 가장 높은 점수를 1등으로 선언
+	cpy[0].rank = 1; //내림차순으로 정렬된 가장 높은 점수를 1등으로 선언
 	for (i = 1; i < N; i++) {
-		if (a[i].score == a[i - 1].score) {
-			a[i].rank = a[i - 1].rank;
+		if (cpy[i].score == cpy[i - 1].score) {
+			cpy[i].rank = cpy[i - 1].rank;
 		}
 		else {
-			a[i].rank = i + 1;
+			cpy[i].rank = i + 1;
+		}
+	}
+	for (i = 0; i < N; i++) { //사본의 등수를 원본에 대입
+		for (j = 0; j < N; j++) { //다중반복문으로 일일히 조건을 검사
+			if (a[i].score == cpy[j].score) {
+				a[i].rank = cpy[j].rank;
+			}
 		}
 	}
 }
@@ -34,10 +44,10 @@ void output()
 	int i, j;
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 
-	fprintf(out, "  이름 점수 등수\n");
+	fprintf(out, "Name Score Rank\n");
 
 	for (i = 0; i < N; i++) {
-		fprintf(out, "%s %4d %4d\n", a[i].name, a[i].score, a[i].rank);
+		fprintf(out, "%4s %5d %4d\n", a[i].name, a[i].score, a[i].rank);
 	}
 
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
