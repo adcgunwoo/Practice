fd6cc534232fb15cc8f26de8332d11e5eb051b19
김건우 <gun2935@naver.com>
2024-12-04 14:28:46 +0900

    feat : 파일 입출력으로 성적표 출력 여러명의 이름과 성적을 입력받고, 등수를 매겨 점수는 내림차순, 등수는 오름차순으로 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,28 +1,64 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-int main()
+#include <string.h>
+int N;
+struct data
 {
-	int a[10],i;
-    //아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
-	FILE *in, *out;  //포인터 변수의 이름은 곧 스트림의 이름이 된다.
-	in = fopen("./temp/input.txt", "r"); //입력 파일스트림을 생성, 
-	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
-	out = fopen("./temp/output.txt", "w"); //출력 파일 스트림을 생성 
-	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결
-	
-	
-	for (i = 0; i < 10; i++) {
-		fscanf(in, "%d", &a[i]);//in과 연결된 파일로부터 자료를 입력받음
+	char name[20];
+	int score, rank, cnt = 0;
+}a[100], tmp;
+void sort()
+{
+	int i, j;
+	for (i = 0; i < N - 1; i++) { //성적 내림차순으로 정렬하기위해 선택정렬
+		for (j = i + 1; j < N; j++) {
+			if (a[i].score < a[j].score) {
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+	}
+	a[0].rank = 1; //내림차순으로 정렬된 가장 높은 점수를 1등으로 선언
+	for (i = 1; i < N; i++) {
+		if (a[i].score == a[i - 1].score) {
+			a[i].rank = a[i - 1].rank;
+		}
+		else {
+			a[i].rank = i + 1;
+		}
+	}
+}
+void output()
+{
+	int i, j;
+	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
+
+	fprintf(out, "  이름 점수 등수\n");
+
+	for (i = 0; i < N; i++) {
+		fprintf(out, "%s %4d %4d\n", a[i].name, a[i].score, a[i].rank);
 	}
 
-	for (i = 1; i < 10; i+=2) {
-		printf("%d ", a[i]);
+	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
+
+}
+int main()
+{
+	int i, j;
+	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
+	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
+	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성, 
+	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
+	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
+	for (N = 0; ; N++) {
+		if (fscanf(in, "%s %d", a[N].name, &a[N].score) < 2) {//입력받은 자료의 개수를 반환하는데
+			break;                                    //파일이 끝나면 2개 미만이 반환되므로 종료
+		}
 	}
-	for (i = 1; i < 10; i+=2) { //입력과 방법이 같음,똑같이 출력
-		fprintf(out, "%d ", a[i]);
-	}
-	fclose(in); //함수가 종료될때 닫히지만
-	fclose(out); //예기치 않게 중단될때를 대비해 작성해야함.
+	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
+	sort();
+	output();
 
 	return 0;
 }
