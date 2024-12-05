c44f68b0ddceb8fcb4fbce642787b8f953591576
김건우 <gun2935@naver.com>
2024-12-05 16:42:39 +0900

    feat : 선택정렬, 구조체를 활용 in 파일입출력 형성평가 8 설명은 따로 없다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,24 +1,38 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <string.h>
+struct data
+{
+	int score[3],sum;
+}a[100];
 int main()
 {
-	int i,j,a,b,cnt=0;
-
+	int n,i,j;
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
 	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	fscanf(in, "%d %d", &a,&b);
-	for (i = a; i <= b; i++) {
-		if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) {
-			cnt+=1;
+	fscanf(in, "%d", &n);
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < 3; j++) {
+			fscanf(in, "%d", &a[i].score[j]);
+			a[i].sum += a[i].score[j];
 		}
 	}
-	fprintf(out, "%d", cnt);
-
+	for (i = 0; i < n - 1; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (a[i].sum < a[j].sum) {
+				int tmp = a[i].sum;
+				a[i].sum = a[j].sum;
+				a[j].sum = tmp;
+			}
+		}
+	}
+	for (i = 0; i < n; i++) {
+		fprintf(out, "%.1lf\n", a[i].sum / 3.0);
+	}
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	return 0;
