d2d8cdf70a0db8c6d01fe12e2bfcfd5134f9e7d8
김건우 <gun2935@naver.com>
2024-12-05 15:33:46 +0900

    feat : 정수 배열의 파일입출력 in 형성평가5 정수 배열을 0을 입력받기 전까지 입력받다가, 15의 배수와 그 개수를 출력

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 int main()
 {
-	int i,n,fac[11]={1,1,};
+	int i,a[1000],cnt=0,cnt_=0;
 
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
@@ -11,11 +11,22 @@ int main()
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	fscanf(in, "%d", &n);
-	for (i = 1; i <= n; i++) {
-		fac[i] = i * fac[i - 1];
+	
+	for (i = 0; i < 1000; i++) {
+		fscanf(in,"%d", &a[i]);
+		if (a[i] == 0) {
+			break;
+		}
+		cnt++;
 	}
-	fprintf(out, "%d", fac[n]);
+	for (i = 0; i < cnt; i++) {
+		if (a[i] % 15 == 0) {
+			fprintf(out, "%d ", a[i]);
+			cnt_++;
+		}
+	}
+	
+	fprintf(out, "\n%d", cnt_);
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	return 0;
