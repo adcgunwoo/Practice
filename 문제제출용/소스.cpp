d9591e25770fb4f1e5b5b3e946bdb51142771ac0
김건우 <gun2935@naver.com>
2024-12-05 16:15:20 +0900

    feat : 파일입출력으로 실행한 별모형 in 형성평가 6 다중반복문으로 공백과 별을 출력해 마름모 모양을 만들었다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 int main()
 {
-	int i,a[1000],cnt=0,cnt_=0;
+	int i,j,a;
 
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
@@ -11,22 +11,26 @@ int main()
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	
-	for (i = 0; i < 1000; i++) {
-		fscanf(in,"%d", &a[i]);
-		if (a[i] == 0) {
-			break;
+	fscanf(in, "%d", &a);
+	for (i = 1; i <= a; i++) {
+		for (j = 1; j <= 2*(a-i); j++) {
+			fprintf(out, " ");
 		}
-		cnt++;
-	}
-	for (i = 0; i < cnt; i++) {
-		if (a[i] % 15 == 0) {
-			fprintf(out, "%d ", a[i]);
-			cnt_++;
+		for (j = 1; j <= 2 * i - 1; j++) {
+			fprintf(out, "* ");
 		}
+		fprintf(out, "\n");
+	}
+	for (i = 1; i <= a-1; i++) {
+		for (j = 1; j <= i*2; j++) {
+			fprintf(out, " ");
+		}
+		for (j = 1; j <= 2*(a-i)-1; j++) {
+			fprintf(out, "* ");
+		}
+		fprintf(out, "\n");
 	}
 	
-	fprintf(out, "\n%d", cnt_);
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	return 0;
