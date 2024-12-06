89e1b2fbc35387c8a11a36701dd54ddec002009c
김건우 <gun2935@naver.com>
2024-12-06 15:29:32 +0900

    feat : 윤년의 개수 함수를 호출하여 조건문과 반복문을 통해 윤년의 개수를 구할 수 있겠지만, 범위가 크다면 굉장히 오래 걸린다. 하지만 이러한 방식도 있다는 것을 기록하려 한다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,36 +1,17 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
-#include <string.h>
-int main()
-{
-	char word[10][100],n,tmp[100];
-	int i,j,len[10];
-	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
-	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
-	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성
-	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
-	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
-	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	for (i = 0; i < 10; i++) {
-		fscanf(in, "%s", word[i]);
-		len[i] = strlen(word[i]);
-	}
-	fscanf(in, " %c", &n);
-	for (i = 0; i < 9; i++) {
-		for (j = i + 1; j < 10; j++) {
-			if (strcmp(word[i], word[j]) > 0) {
-				strcpy(tmp, word[i]);
-				strcpy(word[i], word[j]);
-				strcpy(word[j], tmp);
-			}
-		}
-	}
-	for (i = 0; i < 10; i++) {
-		if (strchr(word[i], n)) {
-			fprintf(out, "%s\n", word[i]);
-		}
-	}
-	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
-	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
-	return 0;
+
+int countLeapYears(int up_to) { // 1부터 입력받은 값 까지의 윤년의 개수
+    return (up_to / 4) - (up_to / 100) + (up_to / 400);
+}
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // 윤년 개수를 계산하는 방법
+    int result = countLeapYears(a) - countLeapYears(b+1);
+
+    printf("%d\n", result);
+
+    return 0;
 }
