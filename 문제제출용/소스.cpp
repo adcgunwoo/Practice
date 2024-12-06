f9813fa344289e1cb4ba8aa5c63d413bc46da378
김건우 <gun2935@naver.com>
2024-12-06 14:39:12 +0900

    feat : 파일입출력과 C언어 교재 끝 문자열 10개와 문자 하나를 입력 받고 단어 순으로 출력해야 하기에 먼저 정렬을 하고, strchr 검사를 통해 해당 문자를 포함하면 출력한다. 리턴함수와 같이 복습해야할 것들이 많았지만, 한번 더 돌리니까 확실히 쉬워졌다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -3,39 +3,31 @@
 #include <string.h>
 int main()
 {
-	int i, j,wcnt=0,wlen=0, tmp;
-	char a[100],word[100][100];
+	char word[10][100],n,tmp[100];
+	int i,j,len[10];
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
 	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	fgets(a, 100, in);
-	int len = strlen(a);
-	for (i = 0; i < len; i++) {
-		if (a[i] == ' ') {
-			word[wcnt][wlen] = '\0';
-			wcnt++;
-			wlen = 0;
-		}
-		else {
-			word[wcnt][wlen] = a[i];
-			wlen++;
+	for (i = 0; i < 10; i++) {
+		fscanf(in, "%s", word[i]);
+		len[i] = strlen(word[i]);
+	}
+	fscanf(in, " %c", &n);
+	for (i = 0; i < 9; i++) {
+		for (j = i + 1; j < 10; j++) {
+			if (strcmp(word[i], word[j]) > 0) {
+				strcpy(tmp, word[i]);
+				strcpy(word[i], word[j]);
+				strcpy(word[j], tmp);
+			}
 		}
 	}
-	word[wcnt][wlen] = '\0';
-	wcnt++;
-	tmp = strlen(word[0]);
-	for (i = 0; i < wcnt; i++) {
-		if (tmp < strlen(word[i])) {
-			tmp = strlen(word[i]);
-		}
-	}
-	fprintf(out, "%d\n", len);
-	for (i = 0; i < wcnt; i++) {
-		if (tmp == strlen(word[i])) {
-			fprintf(out, "%s ", word[i]);
+	for (i = 0; i < 10; i++) {
+		if (strchr(word[i], n)) {
+			fprintf(out, "%s\n", word[i]);
 		}
 	}
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
