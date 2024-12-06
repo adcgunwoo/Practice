7c1b43a07be310d9d36a94b8302cdeb97224b9b3
김건우 <gun2935@naver.com>
2024-12-06 14:18:43 +0900

    feat : 파일입출력으로 공백이 있는 문장을 입력받고, 문장의 길이와 길이가 가장 긴 단어들을 출력 단어를 저장하기 위한 이차원 배열을 선언하고, 행과 줄을 늘려가면서 입력받았다. 공백이 있으면 널값을 입력하고, 행을 늘린다.

--- 문제제출용/소스.cpp
+++ 문제제출용/소스.cpp
@@ -1,37 +1,42 @@
 #define _crt_no_secure_warnings
 #include <stdio.h>
 #include <string.h>
-struct data
-{
-	int score[3],sum;
-}a[100];
 int main()
 {
-	int n,i,j;
+	int i, j,wcnt=0,wlen=0, tmp;
+	char a[100],word[100][100];
 	//아래에서 파일 입출력 스트림을 생성하기 위해 파일포인터를 선언
 	//포인터 변수의 이름은 곧 스트림의 이름이 된다.
 	FILE* in = fopen("input.txt", "r"); //입력 파일스트림을 생성
 	FILE* out = fopen("output.txt", "w"); //출력 파일 스트림을 생성
 	//input.txt을 읽기 전용으로 오픈하고, in이라는 스트림을 이용해 연결
 	//output.txt을 출력 전용으로 오픈하고, out이라는 스트림을 이용하여 연결	
-	fscanf(in, "%d", &n);
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < 3; j++) {
-			fscanf(in, "%d", &a[i].score[j]);
-			a[i].sum += a[i].score[j];
+	fgets(a, 100, in);
+	int len = strlen(a);
+	for (i = 0; i < len; i++) {
+		if (a[i] == ' ') {
+			word[wcnt][wlen] = '\0';
+			wcnt++;
+			wlen = 0;
+		}
+		else {
+			word[wcnt][wlen] = a[i];
+			wlen++;
 		}
 	}
-	for (i = 0; i < n - 1; i++) {
-		for (j = i + 1; j < n; j++) {
-			if (a[i].sum < a[j].sum) {
-				int tmp = a[i].sum;
-				a[i].sum = a[j].sum;
-				a[j].sum = tmp;
-			}
+	word[wcnt][wlen] = '\0';
+	wcnt++;
+	tmp = strlen(word[0]);
+	for (i = 0; i < wcnt; i++) {
+		if (tmp < strlen(word[i])) {
+			tmp = strlen(word[i]);
 		}
 	}
-	for (i = 0; i < n; i++) {
-		fprintf(out, "%.1lf\n", a[i].sum / 3.0);
+	fprintf(out, "%d\n", len);
+	for (i = 0; i < wcnt; i++) {
+		if (tmp == strlen(word[i])) {
+			fprintf(out, "%s ", word[i]);
+		}
 	}
 	fclose(in); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
 	fclose(out); //함수가 종료될때 닫히지만 예기치 않게 중단될때를 대비해 작성해야함.
